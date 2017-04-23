ed467f76f48d3ca7c4ebe0e4ef3c7413cd13ed60
pavel <buhmatov@gmail.com>
2017-04-23 17:34:57 +0300

    Added memento pattern example
    Signed-off-by: pavel <buhmatov@gmail.com>

---
+++ src/main/java/learntocode/patterns/memento/Caretaker.java
@@ -0,0 +1,17 @@
+package learntocode.patterns.memento;
+
+/**
+ * Caretaker class can save {@link Memento} objects, but can not create them.
+ * Only {@link Originator} can create memento object and only {@link Caretaker} can retrieve them.
+ */
+public class Caretaker {
+    private Memento memento;
+
+    public String getSavedState() {
+        return memento.getState();
+    }
+
+    public void setMemento(Memento memento) {
+        this.memento = memento;
+    }
+}
