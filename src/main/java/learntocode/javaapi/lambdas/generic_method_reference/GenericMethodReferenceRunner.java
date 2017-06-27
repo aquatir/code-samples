229b66622393611d2e94fb6d747b1315df6bc69a
pavel <p.bukhmatov@roseurobank.ru>
2017-06-27 11:19:10 +0300

    Adding better commentary to generic reference example
    Signed-off-by: pavel <p.bukhmatov@roseurobank.ru>

--- src/main/java/learntocode/javaapi/lambdas/generic_method_reference/GenericMethodReferenceRunner.java
+++ src/main/java/learntocode/javaapi/lambdas/generic_method_reference/GenericMethodReferenceRunner.java
@@ -6,8 +6,11 @@ public class GenericMethodReferenceRunner {
         Integer[] array = {1,2,6,3,1,256,5,4,2,3,4,6,1,2,3,2};
         Integer value = 4;
 
-        /* here we have reference to generic method in ArrayOps class */
+        /* here we have reference to generic method in ArrayOps class
+        * Also note: we do not actually need to specify generic type here, as java
+        * is smart enough to resolve type by itself. This is done for demonstration
+        * purpose only */
         int count = ArrayOps.performFunction(ArrayOps::<Integer>countMatching, array, value);
-        System.out.println(count);
+
     }
 }
