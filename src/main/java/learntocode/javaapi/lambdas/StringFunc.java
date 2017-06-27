7b494a1a50ebcb48d31ece2a56cab50dc90e13eb
pavel <p.bukhmatov@roseurobank.ru>
2017-06-27 14:47:01 +0300

    Adding disclaimer to lambdas/StringFunc
    You probably don't  need to use simple functional interafaces like the one
    used in StringFunc. Instead you should probably use some of the
    inerfaces that are already defined in java.util.function package if an
    adequate interaface is defined there.

--- src/main/java/learntocode/javaapi/lambdas/StringFunc.java
+++ src/main/java/learntocode/javaapi/lambdas/StringFunc.java
@@ -1,7 +1,12 @@
 package learntocode.javaapi.lambdas;
 
 /**
- * Basic functional interface for custom string functions
+ * Basic functional interface for custom string functions.
+ * <br>
+ * NOTE: You should never create functional interface with a simple signature like this one
+ * because Java already provide it in {@link java.util.function.UnaryOperator} interface.
+ * <br>
+ * Check other interfaces in this package to see standard interfaces Java provides.
  */
 public interface StringFunc {
     String call(String str);
