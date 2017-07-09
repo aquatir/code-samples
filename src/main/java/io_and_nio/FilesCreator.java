768ffba2a82b9d70518a92bc16ab3621c37cc616
pavel <buhmatov@gmail.com>
2017-07-09 14:00:59 +0300

    Add io package with file filter example for list()

---
+++ src/main/java/io_and_nio/FilesCreator.java
@@ -0,0 +1,50 @@
+package io_and_nio;
+
+import java.io.File;
+import java.io.IOException;
+
+public class FilesCreator {
+
+    private static String dirIOFiles = "/tmp/test_files_io";
+    private static String dirNIOFiles = "/tmp/test_files_nio";
+
+    private static String fileNameTemplate = "file_";
+
+    public static String getDirIOFiles() { return dirIOFiles; }
+    public static String getDirNIOFiles() { return dirNIOFiles; }
+    public static String getFileNameTemplate() { return fileNameTemplate; }
+
+
+
+    public static void createFilesIO () {
+        createFiles(dirIOFiles);
+    }
+
+    public static void createFilesNIO() {
+        createFiles(dirNIOFiles);
+    }
+
+    private static void createFiles(String dir) {
+        int numOfFiles = 10;
+
+        File directoryFile = new File(dirIOFiles);
+        directoryFile.mkdirs();
+
+        for (int i =0; i < numOfFiles; i++) {
+            try {
+                File f = new File(dirIOFiles, fileNameTemplate + i);
+                if (f.exists()) {
+                    f.delete();
+                }
+                boolean fileCreated = f.createNewFile();
+                if (!fileCreated) {
+                    System.out.println("Could not create file " + fileNameTemplate + i + " in directory " + dirIOFiles);
+                    break;
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ src/main/java/io_and_nio/io/FileFilters.java
@@ -0,0 +1,31 @@
+package io_and_nio.io;
+
+import io_and_nio.FilesCreator;
+
+import java.io.File;
+import java.util.Arrays;
+
+/**
+ * Class assumes that test files were already created with {@link FilesCreator}
+ */
+public class FileFilters {
+    public static void main(String[] args) {
+
+        FilesCreator.createFilesIO();
+        String dir = FilesCreator.getDirIOFiles();
+        String template = FilesCreator.getFileNameTemplate();
+
+        File startingDir = new File(dir);
+
+        String[] listOfDirs = startingDir.list((dir1, name) -> {
+
+            /* cut file name 'file_xxx' to 'xxx' then parse it as integer
+            * Leave out only files which ends with even numbers */
+            int fileNum = Integer.parseInt(name.substring(name.indexOf("_") + 1));
+            return (fileNum % 2) == 0;
+        });
+
+        Arrays.sort(listOfDirs);
+        Arrays.stream(listOfDirs).forEach(c -> System.out.println(c));
+    }
+}
