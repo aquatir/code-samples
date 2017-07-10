0c8469600cbf58cc88d615e23dd08bf84e701af2
pavel <p.bukhmatov@roseurobank.ru>
2017-07-10 11:23:36 +0300

    Create file examples should now work both for linux and windows

--- src/main/java/learntocode/javaapi/io_and_nio/FilesCreator.java
+++ src/main/java/learntocode/javaapi/io_and_nio/FilesCreator.java
@@ -3,10 +3,16 @@ package learntocode.javaapi.io_and_nio;
 import java.io.File;
 import java.io.IOException;
 
+/**
+ * Provide methods to create directories with files on you machine.
+ * Is used in every class io and nio packages examples
+ * <br>
+ * Change directories in this file to ones you like better
+ */
 public class FilesCreator {
 
-    private static String dirIOFiles = "/tmp/test_files_io";
-    private static String dirNIOFiles = "/tmp/test_files_nio";
+    private static String dirIOFiles;
+    private static String dirNIOFiles;
 
     private static String fileNameTemplate = "file_";
 
@@ -14,9 +20,8 @@ public class FilesCreator {
     public static String getDirNIOFiles() { return dirNIOFiles; }
     public static String getFileNameTemplate() { return fileNameTemplate; }
 
-
-
     public static void createFilesIO () {
+        setDirsByOs();
         createFiles(dirIOFiles);
     }
 
@@ -36,15 +41,37 @@ public class FilesCreator {
                 if (f.exists()) {
                     f.delete();
                 }
+                System.out.print("Creating file " + f.getAbsolutePath() + " ... ");
                 boolean fileCreated = f.createNewFile();
                 if (!fileCreated) {
-                    System.out.println("Could not create file " + fileNameTemplate + i + " in directory " + dirIOFiles);
+                    System.out.println(" ERROR! Could not create file " + fileNameTemplate + i + " in directory " + dirIOFiles);
                     break;
+                } else {
+                    System.out.println("Success!");
                 }
-
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
+
+        System.out.println("Initialization finished. Your output: \n");
+    }
+
+
+    private static void setDirsByOs() {
+        String os = System.getProperty("os.name");
+        if (os.startsWith("Windows")) {
+            System.out.println("Current OS is Windows");
+            String homeDrive = System.getProperty("user.home");
+            dirIOFiles = homeDrive + "\\test_files_io";
+            dirNIOFiles = homeDrive + "\\test_files_nio";
+        } else {
+            dirIOFiles = "/tmp/test_files_io";
+            dirNIOFiles = "/tmp/test_files_nio";
+        }
+
+        System.out.println("Setting test files directories to: \n" +
+                "  IO: " + dirIOFiles + "\n" +
+                "  NIO: " + dirNIOFiles);
     }
 }
