337baa8d382e8193d164823cbf5a8079caebaeb0
pavel <p.bukhmatov@roseurobank.ru>
2017-09-25 11:29:40 +0300

    Add stub for single table inheritance

---
+++ src/main/java/learn_to_code/frameworks/hibernate/single_table_inheritance/HibernateRunner.java
@@ -0,0 +1,7 @@
+package learn_to_code.frameworks.hibernate.single_table_inheritance;
+
+public class HibernateRunner {
+    public static void main(String[] args) {
+        //TODO: do something!
+    }
+}
