688e95972816c60ecbd9ec6ef0da8f0767264d03
pavel <p.bukhmatov@roseurobank.ru>
2017-09-27 11:10:43 +0300

    Add batch insert and update examples for hibernate

--- src/main/java/learn_to_code/frameworks/hibernate/single_table_example/HibernateRunner.java
+++ src/main/java/learn_to_code/frameworks/hibernate/single_table_example/HibernateRunner.java
@@ -1,7 +1,12 @@
 package learn_to_code.frameworks.hibernate.single_table_example;
 
 import learn_to_code.frameworks.hibernate.HibernateUtil;
+import org.hibernate.CacheMode;
+import org.hibernate.ScrollMode;
+import org.hibernate.ScrollableResults;
 import org.hibernate.Session;
+
+import java.util.UUID;
 
 /**
  * Example of populating a table with data.
@@ -79,8 +84,57 @@ public class HibernateRunner {
             printTowns(session);
         }
 
+        /* Note that session object cache all object created until you commit them. Because of
+        * that you have to:
+        * 1) set hibernate.jdbc.batch_size  property (see hibernate.cfg.xml)
+        * 2) flush session periodically*/
+        System.out.println();
+        System.out.println("Batch inserts example");
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            session.beginTransaction();
+
+            for (int i = 0; i < 10000; i++) {
+                Town town = new Town(UUID.randomUUID().toString(), i, UUID.randomUUID().toString());
+                session.save(town);
+                if (i % 20 == 0) {
+                    session.flush();
+                    session.clear();
+                }
+            }
+            System.out.println("All towns inserted");
+            session.getTransaction().commit();
+        }
+
+        /* If you have a lot of data to update, it never hurts to use database cursors, like in this example */
+        System.out.println();
+        System.out.println("Batch update example");
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            session.beginTransaction();
+            ScrollableResults towns = session.createQuery("from Town")
+                    .setCacheMode(CacheMode.IGNORE)
+                    .scroll(ScrollMode.FORWARD_ONLY);
+
+            int i = 0;
+            while (towns.next()) {
+                Town town = (Town) towns.get(0);
+                town.setDistance(i+ 10);
+                i++;
+                if (i % 20 == 0) {
+                    session.flush();
+                    session.clear();
+                }
+            }
+            System.out.println("All towns updated");
+            session.getTransaction().commit();
+        }
+
+
         HibernateUtil.getSessionFactory().close();
     }
+
+
+
+
 
     private static void printTowns(Session session) {
         System.out.println("Printing list of towns:");
