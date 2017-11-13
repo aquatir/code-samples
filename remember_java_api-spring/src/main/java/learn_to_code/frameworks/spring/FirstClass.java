1fd37b0d4ad7bc5b2f3758504dbc5f0e399e73ba
pavel <p.bukhmatov@roseurobank.ru>
2017-11-13 16:36:45 +0300

    Add log4j

--- remember_java_api-spring/src/main/java/learn_to_code/frameworks/spring/FirstClass.java
+++ remember_java_api-spring/src/main/java/learn_to_code/frameworks/spring/FirstClass.java
@@ -1,5 +1,15 @@
 package learn_to_code.frameworks.spring;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
 public class FirstClass {
 
+    private static final Logger log = LogManager.getLogger(FirstClass.class);
+
+    public static void main(String[] args) {
+
+        FirstClass clazz = new FirstClass();
+        log.info("Hello, world!");
+    }
 }
