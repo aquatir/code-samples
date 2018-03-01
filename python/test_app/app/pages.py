6dc2b4bffb92bf630e4cee7386ae1ad68fc59545
bukhmatovps <buhmatov@gmail.com>
2018-03-01 16:13:42 +0300

    Create flask web app

--- python/test_app/app/pages.py
+++ python/test_app/app/pages.py
@@ -1,23 +1,21 @@
-from flask import Flask
-app = Flask(__name__)
-
+from app import app
 
 @app.route("/")
 def hello_world():
     return "Hello, World!"
 
 
-@app.route("/world")
+@app.route("/world/")
 def world_url():
     return "I'm da king!"
 
 
-@app.route("/<name>")
+@app.route("/<name>/")
 def hello_you(name):
     return "Hello, " + name
 
 
-@app.route("/<int:user_number>")
+@app.route("/<int:user_number>/")
 def add_ap_stuff(user_number):
     return "your number: " + str(user_number) + " multiplied by 10: " + str(user_number*10)
 
