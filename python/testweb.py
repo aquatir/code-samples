f095c64e7bfd4c409e2591b8996f5dd016938669
bukhmatovps <buhmatov@gmail.com>
2018-03-01 15:21:37 +0300

    Fix ignore

--- python/testweb.py
+++ python/testweb.py
@@ -17,7 +17,7 @@ def hello_you(name):
     return "Hello, " + name
 
 
-@app.route("/<int:numberr>")
-def add_ap_stuff(numberr):
-    return "your number: " + str(numberr) + " multiplied by 10: " + str(numberr*10)
+@app.route("/<int:user_number>")
+def add_ap_stuff(user_number):
+    return "your number: " + str(user_number) + " multiplied by 10: " + str(user_number*10)
 
