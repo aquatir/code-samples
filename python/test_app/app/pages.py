5234ba01fb52b9e54baeda42d999b1579873a61f
bukhmatovps <buhmatov@gmail.com>
2018-03-01 16:58:49 +0300

    Add some more python stuff

--- python/test_app/app/pages.py
+++ python/test_app/app/pages.py
@@ -1,4 +1,6 @@
 from app import app
+from flask import render_template
+import urllib.request as requester
 
 @app.route("/")
 def hello_world():
@@ -10,12 +12,18 @@ def world_url():
     return "I'm da king!"
 
 
-@app.route("/<name>/")
+@app.route("/hello/<name>/")
 def hello_you(name):
-    return "Hello, " + name
+    #app.logger.debug("A value for debugging")
+    return render_template('hello.html', name=name)
 
 
 @app.route("/<int:user_number>/")
 def add_ap_stuff(user_number):
     return "your number: " + str(user_number) + " multiplied by 10: " + str(user_number*10)
+
+@app.route("/abc/")
+def query():
+    return requester.urlopen("http://localhost:8000/").read()
+
 
