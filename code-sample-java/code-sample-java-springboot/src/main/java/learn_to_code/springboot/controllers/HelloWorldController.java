9abbdb22b37d35c29d7d0df10fc3ad9bb4bca729
bukhmatovps <buhmatov@gmail.com>
2018-03-02 16:12:10 +0300

    Add logging info

--- code-sample-java/code-sample-java-springboot/src/main/java/learn_to_code/springboot/controllers/HelloWorldController.java
+++ code-sample-java/code-sample-java-springboot/src/main/java/learn_to_code/springboot/controllers/HelloWorldController.java
@@ -1,16 +1,24 @@
 package learn_to_code.springboot.controllers;
 
 import learn_to_code.springboot.props.FooProperties;
+import lombok.extern.jbosslog.JBossLog;
+import lombok.extern.log4j.Log4j;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
+/*
  * Spring does not care if your {@code @Controller} is defined as controller or {@code RestController}. This annotation
  * is there simply to make it easier to developers to understand what are they dealing with
  */
 @RestController
+
+/* Loggin with spring-boot and Lombok is trivial. Simply annotate your class with @Slf4j (or any other logging annotation
+from https://projectlombok.org/features/log for respective log). Spring-boot will autoconfigure required log.
+You can then use normal spring-boot configuration for loggers, e.g. setting logging.file or logging.level */
+@Slf4j
 public class HelloWorldController {
 
     @Autowired
@@ -19,6 +27,7 @@ public class HelloWorldController {
     @GetMapping("/")
     @ResponseBody
     public String index() {
+        log.debug("Logging with slf4j");
         return "Greetings from Spring Boot";
     }
 
