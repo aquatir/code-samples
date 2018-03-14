a37c48e5a482c90ae0ce4a8fc31d7f27ddc3bc1e
bukhmatovps <buhmatov@gmail.com>
2018-03-14 15:42:46 +0300

    Add flask example which receive JSON from POST

--- code-sample-python/http/send_binary_file_post.py
+++ code-sample-python/http/send_binary_file_post.py
@@ -1,6 +1,6 @@
 import requests
 
-url = 'http://localhost:8050/model/excel'
+url = 'http://localhost:5000/model/excel'
 headers = {'Content-Type': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
            'Content-Disposition': 'attachment;filename=ANALYTICS_STATS.xlsx'}
 data = open('/tmp/ANALYTICS_STATS.xlsx', 'rb').read()
@@ -8,3 +8,4 @@ res = requests.post(url=url,
                     data=data,
                     headers=headers)
 print(res)
+
