ddb4cebc3001ee10d342cb7ffcdc1a808821a564
bukhmatovps <buhmatov@gmail.com>
2018-04-12 14:41:19 +0300

    Spring security example

---
+++ code-sample-java/code-sample-java-springboot/src/main/java/learn_to_code/springboot/config/WebSecurityConfig.java
@@ -0,0 +1,44 @@
+package learn_to_code.springboot.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import java.util.Collections;
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/home", "/props/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        List<UserDetails> users = Collections.singletonList(User.withUsername("user")
+                .password("password")
+                .roles("USER")
+                .build());
+
+        return new InMemoryUserDetailsManager(users);
+    }
+}
