76e2bc5eea76b315e255f5d69d87b23beaf9435f
pavel <buhmatov@gmail.com>
2018-04-15 21:22:36 +0300

    Add stub for spring-boot in docker example

---
+++ code-sample-springboot-docker/src/main/java/learn_to_code/controller/AllController.java
@@ -0,0 +1,13 @@
+package learn_to_code.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AllController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello, docker!";
+    }
+}
