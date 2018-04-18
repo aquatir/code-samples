5c5560a44da141c185d40584252ff96c6572fcec
pavel <buhmatov@gmail.com>
2018-04-18 23:41:52 +0300

    Beautify

--- code-sample-angular-java/hello-world/src/app/app.module.ts
+++ code-sample-angular-java/hello-world/src/app/app.module.ts
@@ -1,9 +1,9 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
 import {FormsModule} from "@angular/forms";
-import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
+import {AppComponent} from './app.component';
+import {HeroesComponent} from './heroes/heroes.component';
 
 @NgModule({
   declarations: [
@@ -17,4 +17,5 @@ import { HeroesComponent } from './heroes/heroes.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
