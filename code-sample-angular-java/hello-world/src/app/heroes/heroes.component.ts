7f22696b7a2df3ff56b53acb03f79c050ffb1b56
pavel <buhmatov@gmail.com>
2018-04-18 23:50:55 +0300

    Add list example fron angular tutorial

--- code-sample-angular-java/hello-world/src/app/heroes/heroes.component.ts
+++ code-sample-angular-java/hello-world/src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -8,10 +9,14 @@ import { Hero } from '../hero';
 })
 export class HeroesComponent implements OnInit {
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+
+  heroes = HEROES;
+
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 
   constructor() { }
 
