3075ae87cf7fd2edcd81dcd51364ae8cfaf4ddca
pavel <buhmatov@gmail.com>
2018-04-20 00:12:56 +0300

    Add messaging when hero is selected

--- code-sample-angular-java/hello-world/src/app/heroes/heroes.component.ts
+++ code-sample-angular-java/hello-world/src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import {MessageService} from "../message.service";
 
 @Component({
   selector: 'app-heroes',
@@ -9,7 +10,7 @@ import { HeroService } from '../hero.service';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor(private heroService: HeroService) { }
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   heroes: Hero[];
   selectedHero: Hero;
@@ -27,6 +28,7 @@ export class HeroesComponent implements OnInit {
 
 
   onSelect(hero: Hero): void {
+    this.messageService.add("Hero " + hero.name + " choosen");
     this.selectedHero = hero;
   }
 
