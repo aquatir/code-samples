931d4b5fe6745c8cbafb3534f4debc6671b60689
Pavel Bukhmatov <buthmatov@gmail.com>
2018-07-03 09:38:48 +0300

    fix comments on state pattern

--- code-sample-java/code-sample-java-patterns/src/main/java/patterns/state/RunStatePattern.java
+++ code-sample-java/code-sample-java-patterns/src/main/java/patterns/state/RunStatePattern.java
@@ -2,6 +2,9 @@ package patterns.state;
 
 /**
  * Test class for state pattern
+ *
+ * Notice that we call the same {@link Context#callOperation()} while different code is being executed.
+ * All state switching happens in concrete {@link State} implementations.
  */
 public class RunStatePattern {
     public static void main(String[] args) {
