58465b3032153ee1cc3a6a91fee1b93dd5e01b2b
Pavel Bukhmatov <buthmatov@gmail.com>
2018-07-09 14:06:44 +0300

    add principle example

--- code-sample-java/code-sample-spring-security/src/main/java/learn_to_code/springboot/config/WebSecurityConfig.java
+++ code-sample-java/code-sample-spring-security/src/main/java/learn_to_code/springboot/config/WebSecurityConfig.java
@@ -1,9 +1,7 @@
 package learn_to_code.springboot.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
--- code-sample-java/code-sample-spring-security/src/main/java/learn_to_code/springboot/controller/SecureController.java
+++ code-sample-java/code-sample-spring-security/src/main/java/learn_to_code/springboot/controller/SecureController.java
@@ -1,7 +1,16 @@
 package learn_to_code.springboot.controller;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 @RestController
 public class SecureController {
@@ -9,5 +18,28 @@ public class SecureController {
     @GetMapping("/secure")
     public String helloSecurity() {
         return "hello, dude!";
+    }
+
+    @GetMapping("/secure/user")
+    public ModelAndView secureUser() {
+        ModelAndView mav = new ModelAndView("hello");
+
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        String userName = "";
+        String userPassword = "";
+        Set<GrantedAuthority> authorities = Collections.emptySet();
+        if (principal instanceof UserDetails) {
+            UserDetails userDetails = (UserDetails) principal;
+            userName = userDetails.getUsername();
+            userPassword = userDetails.getPassword();
+            authorities = new HashSet<>(userDetails.getAuthorities());
+        }
+
+        mav.addObject("userName", userName);
+        mav.addObject("userPassword", userPassword);
+        mav.addObject("authorities", authorities);
+
+        return mav;
     }
 }
