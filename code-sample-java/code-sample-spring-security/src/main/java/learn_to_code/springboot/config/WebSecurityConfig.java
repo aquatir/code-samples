256349d82ac976a8de92869ba8e4969a3d69e404
Pavel Bukhmatov <buthmatov@gmail.com>
2018-07-09 13:29:50 +0300

    replace configGlobal with userDetailsService

--- code-sample-java/code-sample-spring-security/src/main/java/learn_to_code/springboot/config/WebSecurityConfig.java
+++ code-sample-java/code-sample-spring-security/src/main/java/learn_to_code/springboot/config/WebSecurityConfig.java
@@ -1,12 +1,15 @@
 package learn_to_code.springboot.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 /**
  * Security configuration classes go here.
@@ -27,13 +30,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .inMemoryAuthentication()
-                .withUser(User.withDefaultPasswordEncoder()
-                        .username("user")
-                        .password("password")
-                        .roles("USER"));
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER").build());
+        return manager;
     }
 }
