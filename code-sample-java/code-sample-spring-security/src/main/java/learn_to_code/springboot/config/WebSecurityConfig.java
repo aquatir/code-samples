3ce6fc39b902631dd31abd3a613857b17896ab81
Pavel Bukhmatov <buthmatov@gmail.com>
2018-07-09 11:41:01 +0300

    fix spring security to v5

--- code-sample-java/code-sample-spring-security/src/main/java/learn_to_code/springboot/config/WebSecurityConfig.java
+++ code-sample-java/code-sample-spring-security/src/main/java/learn_to_code/springboot/config/WebSecurityConfig.java
@@ -1,17 +1,12 @@
 package learn_to_code.springboot.config;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-
-import java.util.Collections;
-import java.util.List;
 
 /**
  * Security configuration classes go here.
@@ -24,25 +19,21 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .formLogin()
-                    .loginPage("/login") /* Where your login form should be. */
-                    .permitAll()
-                .and()
-                    .logout()
-                    .permitAll()
-                .and()
-                    .authorizeRequests().anyRequest().authenticated();
+                .authorizeRequests()
+                .antMatchers("/css/**", "/login").permitAll()
+                .antMatchers("/secure").authenticated()
+                    .and()
+                .formLogin().loginPage("/login").failureUrl("/login-error");
     }
 
-    @Bean
-    @Override
-    public UserDetailsService userDetailsService() {
-        /* What users are available. Usually this thing will query database for example */
-        List<UserDetails> users = Collections.singletonList(User.withUsername("user")
-                .password("password")
-                .roles("USER")
-                .build());
 
-        return new InMemoryUserDetailsManager(users);
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser(User.withDefaultPasswordEncoder()
+                        .username("user")
+                        .password("password")
+                        .roles("USER"));
     }
 }
--- code-sample-java/code-sample-spring-security/src/main/java/learn_to_code/springboot/controller/SecureController.java
+++ code-sample-java/code-sample-spring-security/src/main/java/learn_to_code/springboot/controller/SecureController.java
@@ -1,14 +1,12 @@
 package learn_to_code.springboot.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/secure")
 public class SecureController {
 
-    @GetMapping("/")
+    @GetMapping("/secure")
     public String helloSecurity() {
         return "hello, dude!";
     }
