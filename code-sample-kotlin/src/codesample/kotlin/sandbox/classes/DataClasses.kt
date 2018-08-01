51e29214e3f63f0388a12f36759ffe88e51278ba
Pavel Bukhmatov <buthmatov@gmail.com>
2018-08-01 10:56:32 +0300

    Add data class examle. Also getters/setters are wierd in Kotlin!

---
+++ code-sample-kotlin/src/codesample/kotlin/sandbox/classes/DataClasses.kt
@@ -0,0 +1,17 @@
+package codesample.kotlin.sandbox.classes
+
+data class Dude(val name: String,
+                val surname: String,
+                val age: Int) {
+    val someProp : String
+        get() = "getter is called!"
+}
+
+fun main(args: Array<String>) {
+    val ivan = Dude("Ivan", "Ivanov", 15)
+    println(ivan.toString() + " ivans hash: " + ivan.hashCode())
+
+    /* This may seem like accessing a public field... but it's not! This access actually
+    * call a getter on some prop */
+    println(ivan.someProp)
+}
