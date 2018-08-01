de41d7be047d6fd4bd06452a4375afb425de6d49
Pavel Bukhmatov <buthmatov@gmail.com>
2018-08-01 12:11:20 +0300

    Refix typo in comments

--- code-sample-kotlin/src/codesample/kotlin/sandbox/classes/SealedClasses.kt
+++ code-sample-kotlin/src/codesample/kotlin/sandbox/classes/SealedClasses.kt
@@ -6,7 +6,7 @@ data class SealedName(val name: String) : Seal()
 data class SealedAge(val age: Int) : Seal()
 
 /**
- * When dealing with "when expression", you generally have to add else branch.
+ * When dealing with when expression, you generally have to add else branch.
  * When your expression is a sealed class, you don't have to do it, since it's impossible for
  * sealed class to have any value other then that of it's subclasses.
  */
