1c281d471e28a30c3f07af5723350946af46ea91
Pavel Bukhmatov <buthmatov@gmail.com>
2018-08-01 11:59:16 +0300

    Add sealed class example

---
+++ code-sample-kotlin/src/codesample/kotlin/sandbox/classes/SealedClasses.kt
@@ -0,0 +1,24 @@
+package codesample.kotlin.sandbox.classes
+
+sealed class Seal
+
+data class SealedName(val name: String) : Seal()
+data class SealedAge(val age: Int) : Seal()
+
+/**
+ * When dealing with dealing with when expression, you generally have to add else branch.
+ * When your expression is a sealed class, you don't have to do it, since it's impossible for
+ * sealed class to have any value other then that of it's subclasses.
+ */
+fun eval (expression: Seal): String = when (expression) {
+    is SealedName -> "Sealed name"
+    is SealedAge -> "Sealed age"
+}
+
+fun main(args: Array<String>) {
+    val sealedBro = SealedName("Boris")
+    val sealedAge = SealedAge(5)
+    println(eval(sealedBro))
+    println(eval(sealedAge))
+
+}
