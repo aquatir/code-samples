107c39910dd569913c06c2806d8abd1f1f92b1e9
Pavel Bukhmatov <buthmatov@gmail.com>
2018-08-13 18:11:51 +0300

    ADD DESTRUCTURING declaration example

--- code-sample-kotlin/src/codesample/kotlin/sandbox/basics/Lambdas.kt
+++ code-sample-kotlin/src/codesample/kotlin/sandbox/basics/Lambdas.kt
@@ -6,7 +6,6 @@ fun main(args: Array<String>) {
     println("adding 2 to 3 two times, expecting 7, actual: ${applyIntFuncTwice(3, addTwo)}")
 }
 
-
 fun applyIntFuncTwice(a: Int, func: (Int) -> Int) : Int {
     return func(func(a))
 }
