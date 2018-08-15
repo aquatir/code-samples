6e9234483bb9879fd7beab4e7fef2e65fb060696
Pavel Bukhmatov <buthmatov@gmail.com>
2018-08-15 13:07:54 +0300

    Make wired stuff more clear

--- code-sample-kotlin/src/main/kotlin/codesample/kotlin/sandbox/springboot/controller/HelloWorldController.kt
+++ code-sample-kotlin/src/main/kotlin/codesample/kotlin/sandbox/springboot/controller/HelloWorldController.kt
@@ -6,7 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class HelloWorldController (val customerRepository: CustomerRepository){
+class HelloWorldController (
+        val customerRepository: CustomerRepository
+) {
 
     @GetMapping("/")
     fun helloWorld() :String {
