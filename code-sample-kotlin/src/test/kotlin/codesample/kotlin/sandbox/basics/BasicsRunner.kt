a2423e575ec6c67e715618dac2d58ce0d1d2eec2
Pavel Bukhmatov <buthmatov@gmail.com>
2018-08-15 12:35:03 +0300

    Add comments to kotlin spring boot

--- code-sample-kotlin/src/test/kotlin/codesample/kotlin/sandbox/basics/BasicsRunner.kt
+++ code-sample-kotlin/src/test/kotlin/codesample/kotlin/sandbox/basics/BasicsRunner.kt
@@ -3,8 +3,14 @@ package codesample.kotlin.sandbox.basics
 import codesample.kotlin.sandbox.classes.*
 import org.junit.Test
 
+
 class BasicsRunner {
 
+    /**
+     * The runners here can be started in IDEA by clicking on 'run test' button.
+     * These examples are defined in such weird way to NOT specify main class
+     * for spring-boot example (You CAN specify it, but most spring-boot apps don't do it)
+     */
     @Test fun runHelloKotlin() = helloKotlin()
     @Test fun runHelloKotlinCollectionsTest() = helloKotlinCollections()
     @Test fun runLambdas() = lambdas()
