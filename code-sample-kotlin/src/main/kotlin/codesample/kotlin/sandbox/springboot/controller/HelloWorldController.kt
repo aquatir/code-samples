08749bee7a4fe2d312a88f1347230804d5a22191
Pavel Bukhmatov <buthmatov@gmail.com>
2018-08-15 13:02:59 +0300

    Add example for repository injection

--- code-sample-kotlin/src/main/kotlin/codesample/kotlin/sandbox/springboot/controller/HelloWorldController.kt
+++ code-sample-kotlin/src/main/kotlin/codesample/kotlin/sandbox/springboot/controller/HelloWorldController.kt
@@ -1,13 +1,20 @@
 package codesample.kotlin.sandbox.springboot.controller
 
+import codesample.kotlin.sandbox.springboot.db.CustomerRepository
+import codesample.kotlin.sandbox.springboot.entity.Customer
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class HelloWorldController {
+class HelloWorldController (val customerRepository: CustomerRepository){
 
     @GetMapping("/")
     fun helloWorld() :String {
         return "Hello, world!"
+    }
+
+    @GetMapping("/lastNameBauer")
+    fun getBauers() : List<Customer> {
+        return customerRepository.findByLastName("Bauer")
     }
 }
--- code-sample-kotlin/src/main/kotlin/codesample/kotlin/sandbox/springboot/db/CustomerRepository.kt
+++ code-sample-kotlin/src/main/kotlin/codesample/kotlin/sandbox/springboot/db/CustomerRepository.kt
@@ -4,5 +4,5 @@ import codesample.kotlin.sandbox.springboot.entity.Customer
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface CustomerRepository : JpaRepository<Customer, Long> {
-    fun findByLastName(name: String) : List<String>
+    fun findByLastName(name: String) : List<Customer>
 }
