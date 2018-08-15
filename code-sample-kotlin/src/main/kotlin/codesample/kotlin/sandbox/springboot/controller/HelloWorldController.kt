f803c130b76b826f773e09d7ff6ae034f6d5d1e7
Pavel Bukhmatov <buthmatov@gmail.com>
2018-08-15 16:47:29 +0300

    requests in more kotlin way

--- code-sample-kotlin/src/main/kotlin/codesample/kotlin/sandbox/springboot/controller/HelloWorldController.kt
+++ code-sample-kotlin/src/main/kotlin/codesample/kotlin/sandbox/springboot/controller/HelloWorldController.kt
@@ -10,24 +10,20 @@ class HelloWorldController (
 ) {
 
     @GetMapping("/")
-    fun helloWorld() :String {
-        return "Hello, world!"
-    }
+    fun helloWorld() :String
+            = "Hello, world!"
 
     @GetMapping("/all")
-    fun findAll() : List<Customer> {
-        return customerRepository.findAll()
-    }
+    fun findAll() : List<Customer>
+            = customerRepository.findAll()
+
 
     @GetMapping("/lastNameBauer")
-    fun getBauers() : List<Customer> {
-        return customerRepository.findByLastName("Bauer")
-    }
+    fun getBauers() : List<Customer>
+            = customerRepository.findByLastName("Bauer")
 
     @PostMapping("/customer")
-    fun getCustomer(@RequestParam name: String) : List<Customer>  {
-        return customerRepository.findByFirstName(name)
-    }
-
+    fun getCustomer(@RequestParam name: String) : List<Customer>
+            = customerRepository.findByFirstName(name)
 
 }
