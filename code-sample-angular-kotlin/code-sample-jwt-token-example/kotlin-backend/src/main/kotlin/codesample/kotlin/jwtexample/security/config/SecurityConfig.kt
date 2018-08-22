0777be0db1212f8cc72b04c9e64c3b402564adc5
Pavel Bukhmatov <buthmatov@gmail.com>
2018-08-22 15:22:47 +0300

    Add basic auth classes

---
+++ code-sample-angular-kotlin/code-sample-jwt-token-example/kotlin-backend/src/main/kotlin/codesample/kotlin/jwtexample/security/config/SecurityConfig.kt
@@ -0,0 +1,31 @@
+package codesample.kotlin.jwtexample.security.config
+
+import codesample.kotlin.jwtexample.security.AuthExceptionsEntry
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.http.SessionCreationPolicy
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig (val authExceptionsEntry: AuthExceptionsEntry)
+    : WebSecurityConfigurerAdapter() {
+
+    override fun configure(http: HttpSecurity) {
+        http
+                // Route all auth exceptions to this class. See it's comment for more info
+                .exceptionHandling().authenticationEntryPoint(authExceptionsEntry)
+                .and()
+
+                // Do not store any session info. We are going to authenticate each request with JWT token
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+
+                .authorizeRequests()
+
+                .antMatchers("/h2-console/**/**").permitAll()
+                .antMatchers("/auth/**").permitAll()
+                .anyRequest().authenticated()
+    }
+}
