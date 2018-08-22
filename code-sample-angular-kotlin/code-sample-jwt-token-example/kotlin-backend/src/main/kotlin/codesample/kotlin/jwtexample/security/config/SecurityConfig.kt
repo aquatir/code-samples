20df724e1d905d1834e54958115b80565ae618c1
Pavel Bukhmatov <buthmatov@gmail.com>
2018-08-22 15:37:58 +0300

    Fix h2-console being blocked by spring-security

--- code-sample-angular-kotlin/code-sample-jwt-token-example/kotlin-backend/src/main/kotlin/codesample/kotlin/jwtexample/security/config/SecurityConfig.kt
+++ code-sample-angular-kotlin/code-sample-jwt-token-example/kotlin-backend/src/main/kotlin/codesample/kotlin/jwtexample/security/config/SecurityConfig.kt
@@ -3,6 +3,7 @@ package codesample.kotlin.jwtexample.security.config
 import codesample.kotlin.jwtexample.security.AuthExceptionsEntry
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.http.SessionCreationPolicy
@@ -24,8 +25,12 @@ class SecurityConfig (val authExceptionsEntry: AuthExceptionsEntry)
 
                 .authorizeRequests()
 
-                .antMatchers("/h2-console/**/**").permitAll()
                 .antMatchers("/auth/**").permitAll()
                 .anyRequest().authenticated()
+    }
+
+    override fun configure(web: WebSecurity) {
+        web
+                .ignoring().antMatchers("/h2-console/**/**")
     }
 }
