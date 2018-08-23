f5739389c2f4eddcb7471bfd14a076042449e2f1
Pavel Bukhmatov <buthmatov@gmail.com>
2018-08-23 15:27:02 +0300

    Trying to add test to kotlin auth app

---
+++ code-sample-angular-kotlin/code-sample-jwt-token-example/kotlin-backend/src/test/kotlin/codesample/kotlin/jwtexample/security/AuthTest.kt
@@ -0,0 +1,26 @@
+package codesample.kotlin.jwtexample.security
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
+
+@RunWith(SpringJUnit4ClassRunner::class)
+@SpringBootTest
+@AutoConfigureMockMvc
+class AuthTest {
+
+    @Autowired
+    lateinit var mockMvc: MockMvc
+
+    @Test
+    fun authTest() {
+        mockMvc.perform(get("/auth"))
+                .andDo(print())
+    }
+}
