e4c84a1a8d4e0fef4177eaeb86c614674c0aa648
pavel <pbuhmatov@neoflex.ru>
2018-10-05 17:28:02 +0300

    Make ignite example work!

--- code-sample-java/code-sample-ignite-postgres-backstore/src/main/java/codesample/ignite/config/IgniteConfig.java
+++ code-sample-java/code-sample-ignite-postgres-backstore/src/main/java/codesample/ignite/config/IgniteConfig.java
@@ -3,16 +3,56 @@ package codesample.ignite.config;
 import org.apache.ignite.Ignite;
 import org.apache.ignite.IgniteException;
 import org.apache.ignite.Ignition;
+import org.apache.ignite.configuration.IgniteConfiguration;
+import org.apache.ignite.spi.communication.CommunicationSpi;
+import org.apache.ignite.spi.communication.tcp.TcpCommunicationSpi;
+import org.apache.ignite.spi.discovery.DiscoverySpi;
+import org.apache.ignite.spi.discovery.tcp.TcpDiscoverySpi;
+import org.apache.ignite.spi.discovery.tcp.ipfinder.multicast.TcpDiscoveryMulticastIpFinder;
+import org.apache.ignite.spi.discovery.tcp.ipfinder.vm.TcpDiscoveryVmIpFinder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Collections;
 
 @Configuration
 public class IgniteConfig {
 
     @Bean
     Ignite igniteClient() throws IgniteException {
+        return Ignition.start(igniteConfiguration());
+    }
 
-        /* This will search claspath and $IGNITE_HOME s*/
-        return Ignition.start("ignite/client-ignite.xml");
+    @Bean
+    public IgniteConfiguration igniteConfiguration() {
+        IgniteConfiguration cfg = new IgniteConfiguration();
+        cfg.setPeerClassLoadingEnabled(true);
+        cfg.setClientMode(true);
+        cfg.setDiscoverySpi(discoverySpi());
+        cfg.setCommunicationSpi(communicationSpi());
+        return cfg;
+    }
+
+    @Bean
+    public DiscoverySpi discoverySpi() {
+
+        TcpDiscoverySpi discoverySpi = new TcpDiscoverySpi();
+        discoverySpi.setIpFinder(tcpDiscoveryVmIpFinder());
+
+        return discoverySpi;
+    }
+
+    @Bean
+    public TcpDiscoveryVmIpFinder tcpDiscoveryVmIpFinder() {
+        TcpDiscoveryMulticastIpFinder ipFinder = new TcpDiscoveryMulticastIpFinder();
+        ipFinder.setAddresses(Collections.singletonList("127.0.0.1:47500..47509"));
+        return ipFinder;
+    }
+
+    @Bean
+    public CommunicationSpi communicationSpi() {
+        TcpCommunicationSpi communicationSpi = new TcpCommunicationSpi();
+        communicationSpi.setMessageQueueLimit(1024);
+        return communicationSpi;
     }
 }
