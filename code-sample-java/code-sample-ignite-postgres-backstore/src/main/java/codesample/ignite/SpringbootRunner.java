4df14a5808e68c2451b47ae78fe5c6c18bd6c0f2
pavel <pbuhmatov@neoflex.ru>
2018-10-05 12:26:45 +0300

    Downgrade to java8

--- code-sample-java/code-sample-ignite-postgres-backstore/src/main/java/codesample/ignite/SpringbootRunner.java
+++ code-sample-java/code-sample-ignite-postgres-backstore/src/main/java/codesample/ignite/SpringbootRunner.java
@@ -1,4 +1,11 @@
 package codesample.ignite;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class SpringbootRunner {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootRunner.class, args);
+    }
 }
