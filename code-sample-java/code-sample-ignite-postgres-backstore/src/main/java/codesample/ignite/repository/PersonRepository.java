045eb00e7b2cf3f94d3b471ad681c8da8363f3cd
pavel <pbuhmatov@neoflex.ru>
2018-10-09 12:10:04 +0300

    More saint way to inject repository in IGNITE example

--- code-sample-java/code-sample-ignite-postgres-backstore/src/main/java/codesample/ignite/repository/PersonRepository.java
+++ code-sample-java/code-sample-ignite-postgres-backstore/src/main/java/codesample/ignite/repository/PersonRepository.java
@@ -4,8 +4,6 @@ import codesample.ignite.entitry.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {
 
