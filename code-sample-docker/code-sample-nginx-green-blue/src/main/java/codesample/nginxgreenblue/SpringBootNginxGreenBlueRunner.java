0cf5e6a9104b78bf2073c964f18511fe2991c45f
pavel_buhmatov <buhmatov@gmail.com>
2018-10-11 23:20:07 +0300

    renew packages. Start green-blue deploy example

---
+++ code-sample-docker/code-sample-nginx-green-blue/src/main/java/codesample/nginxgreenblue/SpringBootNginxGreenBlueRunner.java
@@ -0,0 +1,19 @@
+package codesample.nginxgreenblue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootNginxGreenBlueRunner {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootNginxGreenBlueRunner.class, args);
+    }
+
+    @GetMapping
+    public String hello() {
+        return "Hello!";
+    }
+}
