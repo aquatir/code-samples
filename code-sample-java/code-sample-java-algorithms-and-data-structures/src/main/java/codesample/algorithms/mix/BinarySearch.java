95c48862f608bd1d2f51f565a4a0c532036ffe65
pavel <pbuhmatov@neoflex.ru>
2018-10-16 18:14:30 +0300

    Add stub for binary search example

---
+++ code-sample-java/code-sample-java-algorithms-and-data-structures/src/main/java/codesample/algorithms/mix/BinarySearch.java
@@ -0,0 +1,10 @@
+package codesample.algorithms.mix;
+
+import java.util.List;
+
+public class BinarySearch<T extends Comparable<T>> {
+
+    public static <T> List<T> search (List<T> array) {
+        return null;
+    }
+}
