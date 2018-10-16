0872593a0f3d3d0f267c695230af14f02ada0184
pavel <pbuhmatov@neoflex.ru>
2018-10-16 15:24:21 +0300

    Better package readme

--- code-sample-java/code-sample-java-algorithms-and-data-structures/src/main/java/codesample/algorithms/union/package-info.java
+++ code-sample-java/code-sample-java-algorithms-and-data-structures/src/main/java/codesample/algorithms/union/package-info.java
@@ -1,6 +1,6 @@
 /**
- * There are two types of connect implementations:
- * 1. Focused on speed of UNION operation which creates a connect created two nodes (which may be part of another connect)
- * 2. Focused on speed of CONNECTED operation which find if 2 nodes are areConnected
+ * There are two types of union implementations:
+ * 1. Focused on speed of 'connect' operation which creates a connect created two nodes (which may be part of another connect)
+ * 2. Focused on speed of 'areConnected' operation which finds if 2 nodes are connected
  */
 package codesample.algorithms.union;
