7f027b0b3db4b2bff394ee71ad85bba07f3cd4df
pavel_buhmatov <buhmatov@gmail.com>
2018-10-22 23:02:18 +0300

    Add tail-recursive newton devision

--- code-sample-scala/hello-world/src/main/scala/codesample/scala/helloworld/HelloScala.scala
+++ code-sample-scala/hello-world/src/main/scala/codesample/scala/helloworld/HelloScala.scala
@@ -2,6 +2,7 @@ package codesample.scala.helloworld
 
 object HelloScala extends App {
 
+  // fibonacci sequence calculator
   def fib(fibNumberPosition: Int): Int = {
 
     def fibIterator(iter: Int, currentLesser: Int, currentHigher: Int) : Int = {
@@ -14,6 +15,26 @@ object HelloScala extends App {
     fibIterator(1, 0, 1)
   }
 
+  // Newton's square root func
+  def newtonSqrt(x: Double) : Double = {
+
+    def newtonItr(x: Double, guess: Double): Double = {
+      if (isGoodEnough(x, guess)) guess
+      else newtonItr(x, improve(x, guess))
+    }
+
+    def abs(x: Double) = {if (x < 0) -x else x}
+    def isGoodEnough(x: Double, guess: Double): Boolean = {
+      abs(x - guess*guess) < 0.001 }
+    def improve(x: Double, guess: Double) : Double = {
+      (guess + x/guess) / 2
+    }
+
+    newtonItr(x, 1)
+
+  }
+
   println("Hello, Scala!")
   println(fib(3)) // 1 1 2 3 5 8 13
+  println(newtonSqrt(2))
 }
