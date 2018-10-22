6f15e1c5cc0d23e89ba3548dbd97fa4d3d8b4c43
pavel_buhmatov <buhmatov@gmail.com>
2018-10-22 14:09:19 +0300

    Add fib number func to Scala example

--- code-sample-scala/hello-world/build.sbt
+++ code-sample-scala/hello-world/build.sbt
@@ -3,3 +3,5 @@ name := "hello-world"
 version := "0.1"
 
 scalaVersion := "2.12.7"
+
+libraryDependencies += "org.scalatest" % "scalatest_2.11" % "2.2.6" % "test"
--- code-sample-scala/hello-world/src/main/scala/codesample/scala/helloworld/HelloScala.scala
+++ code-sample-scala/hello-world/src/main/scala/codesample/scala/helloworld/HelloScala.scala
@@ -1,5 +1,19 @@
 package codesample.scala.helloworld
 
 object HelloScala extends App {
+
+  def fib(fibNumberPosition: Int): Int = {
+
+    def fibIterator(iter: Int, currentLesser: Int, currentHigher: Int) : Int = {
+      if (fibNumberPosition == iter)
+        currentHigher
+      else
+        fibIterator(iter + 1, currentHigher, currentLesser + currentHigher)
+    }
+
+    fibIterator(1, 0, 1)
+  }
+
   println("Hello, Scala!")
+  println(fib(3)) // 1 1 2 3 5 8 13
 }
