9179b1f9b920ba6720375f60b1056f9d49f83718
pavel_buhmatov <buhmatov@gmail.com>
2018-10-23 12:34:18 +0300

    Add tailrec sum func

--- code-sample-scala/scala-coursera/src/main/scala/codesample/scala/coursera/course1/week2/HelloScala2.scala
+++ code-sample-scala/scala-coursera/src/main/scala/codesample/scala/coursera/course1/week2/HelloScala2.scala
@@ -1,5 +1,7 @@
 package codesample.scala.coursera.course1.week2
 
+import scala.annotation.tailrec
+
 object HelloScala2 {
 
   def sum(intFunc: Int => Int, left: Int, right: Int) : Int = {
@@ -7,6 +9,17 @@ object HelloScala2 {
       0
     else
       intFunc(left) + sum(intFunc, left + 1, right)
+  }
+
+  def sumTail(f: Int => Int, left: Int, right: Int): Int = {
+
+    @tailrec
+    def loop(left: Int, acc: Int): Int = {
+      if (left > right) acc
+      else loop(left + 1, acc + f(left))
+    }
+
+    loop(left, 0)
   }
 
 
@@ -17,6 +30,11 @@ object HelloScala2 {
 
     println(s"sum from 0 to 10 is: ${sum(id, 0, 10)}")
     println(s"sum of squares from 0 to 10 is ${sum(square, 0, 10)}")
+    println(s"sum of cubes from 0 to 10 is ${sum(x => x * x * x, 0, 10)}")
+
+    println(s"sum from 0 to 10 is: ${sumTail(x => x, 0, 10)}")
+    println(s"sum of squares from 0 to 10 is ${sumTail(x => x * x, 0, 10)}")
+    println(s"sum of cubes from 0 to 10 is ${sumTail(x => x * x * x, 0, 10)}")
   }
 
 
