5b26a5ac68d3c46f6ffbfb75e115d56121ad11b5
pavel_buhmatov <buhmatov@gmail.com>
2018-11-18 01:01:33 +0300

    Add init, removeAt and flatter examples

--- code-sample-scala/scala-coursera/src/main/scala/codesample/scala/coursera/course1/week5/ScalaListsPartOne.scala
+++ code-sample-scala/scala-coursera/src/main/scala/codesample/scala/coursera/course1/week5/ScalaListsPartOne.scala
@@ -24,6 +24,25 @@ object ScalaListsPartOne {
       else head :: tail
   }
 
+  def init[T](xs: List[T]): List[T] = xs match {
+    case List() => throw new Error("init of empty list")
+    case List(x) => List()
+    case y :: ys => y :: init(ys)
+  }
+
+
+  def removeAt[T](n: Int, xs: List[T]) = xs.take(n) ::: xs.drop(n+1)
+
+  def flatten(xs: List[Any]): List[Any] = xs match {
+    case Nil => Nil
+    case head :: tail => (head match {
+      case headValue: List[Any] => flatten(headValue)
+      case headValue: Any => List(headValue)
+    }) ::: flatten(tail)
+
+  }
+
+
   def main(args: Array[String]): Unit = {
     val list = "Keks" :: "Magic" :: "And shit" :: Nil // You can not omit :: Nil
     val otherList = "abc" :: "def" :: Nil
@@ -36,11 +55,24 @@ object ScalaListsPartOne {
     println("Tail of first: " + list.tail)
 
     // Isertion sort!
-    val someList = List(9,5,3,2,1,6,3,2)
+    val someList = List(9, 8, 7, 6, 5, 4, 3, 2, 1, 0)
     println("Insertion sorted list: " + someList + " => " + insSort(someList))
     println("Bubble sorted list: " + someList + " => " + bubbleSort(someList))
     println("All elements but last: " + someList.init)
+    println("My init: " + init(someList))
 
+    println("list without first 5 elements: " + someList.drop(5))
+    println("only first 5 elements of list: " + someList.take(5))
+
+    println("Reversed list: " + someList.reverse)
+    println("Double list" + (someList ++ someList))
+
+    val first = List(1,2,3)
+    val second = List(4,5,6)
+    println("what ::: do? first ::: second: " + (first ::: second))
+
+    println("element removed: " + removeAt(1, List('a', 'b', 'c', 'd'))) // List(a, c, d)
+    println("flattenned: " + flatten(List(List(1, 1), 2, List(3, List(5, 8))))) // 1, 1, 2, 3, 5, 8
 
   }
 
