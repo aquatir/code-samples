19909fad0c6f83d3a087d6690d225d31b8216b8c
pavel <pbuhmatov@neoflex.ru>
2019-02-14 12:19:55 +0300

    Better readme for API versioning

--- code-sample-java/code-sample-spring-api-versioning/src/main/java/codesample/apiversioning/ApiVersioningSpringBootRunner.java
+++ code-sample-java/code-sample-spring-api-versioning/src/main/java/codesample/apiversioning/ApiVersioningSpringBootRunner.java
@@ -6,6 +6,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * BIG NOTICE:
+ *
+ *
+ * You do not actually need to do anything like shown here for header mapping because
+ * You can simply add a header as parameter to @GetMapping.
+ *
+ * However when willing to create extra-customizable request-resolution rules, this implementation may prove useful
+ */
+
 @SpringBootApplication
 @RestController
 public class ApiVersioningSpringBootRunner {
@@ -23,5 +33,10 @@ public class ApiVersioningSpringBootRunner {
     @GetMapping("/")
     public String helloNew() {
         return "Hello 1.7!";
+    }
+
+    @GetMapping("/someOther")
+    public String hey() {
+        return "hey!";
     }
 }
