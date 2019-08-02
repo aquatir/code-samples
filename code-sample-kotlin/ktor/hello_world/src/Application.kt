2900baec688c54e2974f7f7fd80beb3825d500fe
pavel_buhmatov <buhmatov@gmail.com>
2019-08-02 08:08:29 +0300

    [kt]: Remove unused annotations

--- code-sample-kotlin/ktor/hello_world/src/Application.kt
+++ code-sample-kotlin/ktor/hello_world/src/Application.kt
@@ -17,7 +17,6 @@ import io.ktor.client.features.logging.*
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
-@Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.main(testing: Boolean = false) {
     install(Authentication) {
