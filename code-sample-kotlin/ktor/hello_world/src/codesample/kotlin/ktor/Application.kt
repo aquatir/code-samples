3f7585fc4a61cb73158178bd56cb5a2980ef6371
Pavel Bukhmatov <pbukhmatov@oboz.com>
2019-08-09 09:09:33 +0300

    [kt][ktor]: Add kafka producer

--- code-sample-kotlin/ktor/hello_world/src/codesample/kotlin/ktor/Application.kt
+++ code-sample-kotlin/ktor/hello_world/src/codesample/kotlin/ktor/Application.kt
@@ -17,7 +17,12 @@ import io.ktor.client.features.logging.*
 import io.ktor.gson.gson
 import io.ktor.locations.Locations
 import io.ktor.request.queryString
+import org.apache.kafka.clients.producer.KafkaProducer
+import org.apache.kafka.clients.producer.Producer
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.common.serialization.StringSerializer
 import org.yaml.snakeyaml.Yaml
+import java.util.*
 
 
 sealed class Component(open val nodeName: String)
@@ -65,10 +70,21 @@ class PropertyReader(map: MutableMap<String, Any>) {
                 }
             }
         }
-        return if (curNumOfTraversed == targetNumOfTraversed && curPlaceInTree.nodeName == splittedPath.last())
-            curPlaceInTree.nodeName
+        return if (curNumOfTraversed == targetNumOfTraversed && curPlaceInTree.nodeName == splittedPath.last()) {
+
+            return when(curPlaceInTree) {
+                is Leaf -> curPlaceInTree.nodeName // Is reachable?
+                is Composite -> {
+                    curPlaceInTree.getChildren()
+                        .takeIf { it.size == 1 }
+                        ?.first()?.nodeName
+                }
+            }
+        }
         else null
     }
+
+    fun readPropertyOrThrow(path: String): String = readProperty(path)!!
 
     private fun readMapRecursively(composite: Composite, mutableMap: MutableMap<String, Any>) {
         mutableMap.forEach { (key, value) ->
@@ -87,6 +103,15 @@ class PropertyReader(map: MutableMap<String, Any>) {
 
 
 
+private fun createProducer(brokers: String): Producer<String, String> {
+    val props = Properties()
+        .apply {
+            this["bootstrap.servers"] = brokers
+            this["key.serializer"] = StringSerializer::class.java.canonicalName
+            this["value.serializer"] = StringSerializer::class.java.canonicalName
+        }
+    return KafkaProducer(props)
+}
 
 
 
@@ -107,6 +132,7 @@ fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 @kotlin.jvm.JvmOverloads
 fun Application.main(testing: Boolean = false) {
 
+    // CONSUL
     val consulClient = Consul
         .builder()
         .withHostAndPort(HostAndPort.fromString("localhost:8500"))
@@ -125,13 +151,23 @@ fun Application.main(testing: Boolean = false) {
 
     val prop3 = propReaderService.readProperty("specific1/conf1")
     val prop4 = propReaderService.readProperty("specific2")
-    println(common)
-
 
     val resultNum = this.environment.config.propertyOrNull("custom_constant.value_num")?.getString()?.toInt()
     val resultStr = this.environment.config.propertyOrNull("custom_constant.value_str")?.getString()
 
+    println("$prop1, $prop2 $prop3 $prop4 $resultNum $resultStr")
 
+    // KAFKA
+    val producer = createProducer(propReaderCommon.readPropertyOrThrow("kafka/url"))
+
+    val sendResult = producer.send(ProducerRecord("new_topic", "value")).get()
+
+    println("offset: ${sendResult.offset()} partition: ${sendResult.partition()}")
+
+
+
+
+    // Actual application
     install(Authentication) {
     }
 
