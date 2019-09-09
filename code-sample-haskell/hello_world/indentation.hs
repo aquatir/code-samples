f2b506aa57e7d17cf2ffa2df5b50de05a4173881
Pavel Bukhmatov <pbukhmatov@oboz.com>
2019-09-09 15:11:03 +0300

    [hs]: Add module to indentation example

--- code-sample-haskell/hello_world/indentation.hs
+++ code-sample-haskell/hello_world/indentation.hs
@@ -1,6 +1,12 @@
-main = do putStrLn "What is 2 + 2?"
+module Main where
+
+factorial 0 = 1
+factorial n = n * factorial (n-1)
+
+main = do putStrLn "What is factorial of 5?"
           x <- readLn
-          if x == 4
+          if x == factorial 5 
                then putStrLn "You're right!"
                else putStrLn "You'are wrong!"
+
 
