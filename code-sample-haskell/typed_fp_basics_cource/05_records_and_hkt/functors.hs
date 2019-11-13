115f0c988ae6b167d4bd0166f918b6f2bccb7d28
pavel bukhmatov <buhmatov@gmail.com>
2019-11-13 23:39:17 +0300

    tried keepflip...

--- code-sample-haskell/typed_fp_basics_cource/05_records_and_hkt/functors.hs
+++ code-sample-haskell/typed_fp_basics_cource/05_records_and_hkt/functors.hs
@@ -45,7 +45,7 @@ instance Bifunctor TwoPair where
     
 -- Задание 2 (хардкорное)
 -- Реализовать Foldable через fold. Для этого придется изобрести (или подглядеть в Data.Monoid) обёртки-моноиды, так как в сигнатуре fold есть моноид. 
-data List a = Cons a (List a) | Nil
+-- data List a = Cons a (List a) | Nil
 
 
 -- Задание 1 
@@ -57,20 +57,45 @@ data KeepFlip a b
   | Flip b (KeepFlip b a)
   | End
 
+
 instance Monoid (KeepFlip a b) where  
     mempty = End
-    
     mappend a End = a 
     mappend End b = b
-    mappend (Keep _ _) (Keep _ _) = undefined  -- Каждый это еще пачка из 4...?
-    mappend (Flip _ _) (Keep _ _) = undefined
-    mappend (Keep _ _) (Flip _ _) = undefined
-    mappend (Flip _ _) (Flip _ _) = undefined
+    mappend (Keep x xs) ys = Keep x $ mappend xs ys
+    mappend (Flip x xs) (Keep y ys) = Flip x $ mappend xs (Flip y ys)
+    mappend (Flip x xs) (Flip y ys) = Flip x $ mappend xs (Keep y ys)
 
+instance Functor (KeepFlip a) where
+    -- fmap :: (a -> b) -> f a -> f b
+    fmap _ End = End
+    fmap f (Keep x xs) = Keep x (fmap f xs)
+    fmap f (Flip x xs) = undefined  -- ???
+
+
+data MyList a = Cons a (MyList a) | Nil 
+    deriving Show
+
+instance Monoid (MyList a) where
+    mempty = Nil
+    mappend a Nil = a
+    mappend Nil a = a
+    mappend (Cons x Nil) (Cons y ys) = Cons x (Cons y (ys))
+    mappend (Cons x xs) xy = Cons x (mappend (xs) xy)
+
+instance Functor (MyList) where
+    fmap :: (a -> b) -> MyList a -> MyList b
+    fmap _ Nil = Nil    
+    fmap f (Cons x Nil) = Cons (f x) Nil
+    fmap f (Cons x xs)   = Cons (f x) (fmap f xs) 
+     
+    
 
 main = do
     print pair 
     print pairAfterApply
+    print $ mappend (Cons 5 (Cons 18 Nil)) (Cons 10 (Cons 15 Nil)) 
+    print $ fmap (+3) (Cons 5 (Cons 10 Nil))
     where 
         pair = Pair "hello" "world"
         pairAfterApply = fmap (++ "!") $ pair
