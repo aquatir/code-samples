2c21138d3dcd38188a5049b4dda5bede2c1c9614
pavel bukhmatov <buhmatov@gmail.com>
2019-11-14 01:24:38 +0300

    [hs]: add more hskl

---
+++ code-sample-haskell/hello_world/stuff.hs
@@ -0,0 +1,20 @@
+module Stuff where
+
+
+printHello = print "Hello, world"
+double x = x + x
+doubleBothSum x y = double x + double y
+doubleNotAll x = if (x > 100) 
+                 then x
+                 else double x 
+
+someComprehension xs ys = [(x,y) | x <- xs, y <- ys, y /= 5, x /= 3, x > y]
+
+
+-- All right triangles with perimeter of 24 out of all triangles with length of size between 1 and 10
+-- let rightTriangles = [ (a,b,c) | c <- [1..10], a <- [1..c], b <- [1..10], a^2 + b^2 == c^2, a+b+c == 24]
+
+removeNonUppercase :: [Char] -> [Char]  
+removeNonUppercase st = [ c | c <- st, c `elem` (' ' : ['A'..'Z'])] 
+
+
