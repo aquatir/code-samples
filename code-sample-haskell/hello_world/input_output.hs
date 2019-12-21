ce97a8e0ce53ea06159e15e7a05109807d375e22
pavel bukhmatov <buhmatov@gmail.com>
2019-12-21 11:59:55 +0300

    [hs]: Add more input/output

--- code-sample-haskell/hello_world/input_output.hs
+++ code-sample-haskell/hello_world/input_output.hs
@@ -43,6 +43,25 @@ foreverUpper = do
         putStrLn $ "write something" 
         userInput <- getLine
         putStrLn $ map toUpper userInput
+
+contentsStuff = do 
+    forever $ do 
+        stuff <- getContents
+        putStrLn $ map toLower stuff
+
+printOnlyShort = do  
+        contents <- getContents  
+        putStr (shortLinesOnly contents)  
+      
+shortLinesOnly :: String -> String  
+shortLinesOnly input =   
+    let allLines = lines input  
+        shortLines = filter (\line -> length line < 10) allLines  
+        result = unlines shortLines  
+    in  result  
         
+printOnlyShortInteract = interact shortLinesOnly
+printOnlyShortInteractOneLiner = interact $ unlines . filter ((<10) . length) . lines   
+
 reverseWords :: String -> String
 reverseWords = unwords . map reverse . words
