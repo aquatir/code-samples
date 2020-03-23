5dbcdbdc8379c0cb3adec420e8fba2c6417ac054
pavel bukhmatov <buhmatov@gmail.com>
2020-03-23 14:15:13 +0300

    db seems to work

--- code-sample-kotlin/graal-native-image/nakedgraalgrpc/docker-build.sh
+++ code-sample-kotlin/graal-native-image/nakedgraalgrpc/docker-build.sh
@@ -5,4 +5,4 @@ docker build . -t nakedgraalgrpc
 echo
 echo
 echo "To run the docker container execute:"
-echo "    $ docker run -p 50051:50051 nakedgraalgrpc"
+echo "    $ docker run -p 50051:50051 nakedgraalgrpc --net mynet -n app"
--- code-sample-kotlin/graal-native-image/nakedgraalgrpc/src/main/kotlin/codesample/kotlin/graalvm/Application.kt
+++ code-sample-kotlin/graal-native-image/nakedgraalgrpc/src/main/kotlin/codesample/kotlin/graalvm/Application.kt
@@ -1,27 +1,55 @@
 package codesample.kotlin.graalvm
 
+import com.zaxxer.hikari.HikariDataSource
 import io.grpc.ServerBuilder
 import io.grpc.stub.StreamObserver
+import org.jooq.DSLContext
+import org.jooq.SQLDialect
+import org.jooq.impl.DSL
+import javax.sql.DataSource
 
 object Application {
     @JvmStatic
     fun main(args: Array<String>) {
 
+        val ds: DataSource = HikariDataSource().apply {
+            driverClassName = "org.postgresql.Driver"
+            jdbcUrl = "jdbc:postgresql://pg.mynet:5432/test"
+            //jdbcUrl = "jdbc:postgresql://localhost:5432/test"
+            username = "postgres"
+            password = "postgres"
+
+            connectionTestQuery = "SELECT 1"
+            minimumIdle = 50
+            maximumPoolSize = 500
+            isAutoCommit = true
+            loginTimeout = 3
+            addDataSourceProperty("characterEncoding","utf8");
+            addDataSourceProperty("useUnicode","true");
+        }
+
+        val dslContext = DSL.using(ds, SQLDialect.POSTGRES)
+
         val port = 50051
         val server = ServerBuilder.forPort(port)
-                .addService(ExampleEndpoint())
+                .addService(ExampleEndpoint(dslContext))
                 .build()
                 .start()
 
-        println("Server started")
+        println("GRPC Server started at 50051")
         server?.awaitTermination()
     }
 }
 
-class ExampleEndpoint() : ExampleServiceGrpc.ExampleServiceImplBase() {
+class ExampleEndpoint(private val dslContext: DSLContext) : ExampleServiceGrpc.ExampleServiceImplBase() {
     override fun send(request: ExampleRequest, responseObserver: StreamObserver<ExampleReply>) {
 
         println("Got you request")
+
+
+        val result = dslContext.execute("update transactions set amount = 100");
+        println("num of rows $result")
+
         val reply = ExampleReply.newBuilder().setMessage("Hello, gRPC!").build()
         with(responseObserver) {
             onNext(reply)
