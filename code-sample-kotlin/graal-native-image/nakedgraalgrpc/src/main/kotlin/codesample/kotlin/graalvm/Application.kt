8c8c6b3cd7f7ce85bc6334be397d3c6a97efbd43
pavel bukhmatov <buhmatov@gmail.com>
2020-03-24 18:59:43 +0300

    [nakedgraalgrpc]: Trying to add kafka

--- code-sample-kotlin/graal-native-image/nakedgraalgrpc/src/main/kotlin/codesample/kotlin/graalvm/Application.kt
+++ code-sample-kotlin/graal-native-image/nakedgraalgrpc/src/main/kotlin/codesample/kotlin/graalvm/Application.kt
@@ -5,11 +5,23 @@ import com.orbitz.consul.Consul
 import com.zaxxer.hikari.HikariDataSource
 import io.grpc.ServerBuilder
 import io.grpc.stub.StreamObserver
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
+import org.apache.kafka.clients.consumer.Consumer
+import org.apache.kafka.clients.consumer.KafkaConsumer
+import org.apache.kafka.clients.producer.KafkaProducer
+import org.apache.kafka.clients.producer.Producer
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.kafka.common.serialization.StringSerializer
 import org.jooq.DSLContext
 import org.jooq.SQLDialect
 import org.jooq.impl.DSL
 import org.slf4j.LoggerFactory
 import org.yaml.snakeyaml.Yaml
+import java.time.Duration
+import java.util.*
 import javax.sql.DataSource
 
 
@@ -24,7 +36,7 @@ object Application {
         val consulClient = Consul
                 .builder()
                 //.withHostAndPort(HostAndPort.fromString("localhost:8500"))
-                .withHostAndPort(HostAndPort.fromString("nakedgraalgrpc_consul_1.mynet:8500"))
+                .withHostAndPort(HostAndPort.fromString("consul.mynet:8500"))
                 .build()
         val kvClient = consulClient.keyValueClient()
 
@@ -53,10 +65,42 @@ object Application {
         val dslContext = DSL.using(ds, SQLDialect.POSTGRES)
         dslContext.execute("SELECT 1") // load JOOQ
 
+        //val kafkaBrokerUrl = "127.0.0.1:9092"
+        val kafkaBrokerUrl = "broker.mynet:9092"
+
+        var producer: Producer<String, String>? = null
+        try {
+            log.info("Creating kafka")
+            producer = createProducer(kafkaBrokerUrl)
+            log.info("created producer")
+            val consumer = createConsumer(kafkaBrokerUrl)
+                    .apply { this.subscribe(listOf("new_topic")) }
+            log.info("created consumer")
+
+            log.info("About to launch consumer in coroutine")
+            GlobalScope.launch {
+                while (isActive) {
+                    val record = consumer.poll(Duration.ofSeconds(10))
+                    if (record.count() == 0) {
+                        log.info("no new messages")
+                    } else {
+                        record.records("new_topic").forEach {
+                            log.info("received on consumer: ${it.value()}")
+                        }
+                    }
+                    consumer.commitSync()
+                }
+            }
+            log.info("lanuched consumer loop")
+        } catch (ex: Exception) {
+            log.error("got error in kafka", ex)
+        }
+
+
         // Application configuration
         val port = 50051
         val server = ServerBuilder.forPort(port)
-                .addService(ExampleEndpoint(dslContext))
+                .addService(ExampleEndpoint(dslContext, producer))
                 .build()
                 .start()
 
@@ -65,7 +109,7 @@ object Application {
     }
 }
 
-class ExampleEndpoint(private val dslContext: DSLContext) : ExampleServiceGrpc.ExampleServiceImplBase() {
+class ExampleEndpoint(private val dslContext: DSLContext, private val producer: Producer<String, String>?) : ExampleServiceGrpc.ExampleServiceImplBase() {
 
     private val log = LoggerFactory.getLogger(ExampleEndpoint::class.java)
 
@@ -76,6 +120,14 @@ class ExampleEndpoint(private val dslContext: DSLContext) : ExampleServiceGrpc.E
         val result = dslContext.execute("update transactions set amount = 100");
         log.info("num of rows $result")
 
+        log.info("Sending kafka message")
+        if (producer != null) {
+            val sendResult = producer?.send(ProducerRecord("new_topic", "value")).get()
+            log.info("offset: ${sendResult.offset()} partition: ${sendResult.partition()}")
+        } else {
+            log.warn("No producer available. Sending nothing")
+        }
+
         val reply = ExampleReply.newBuilder().setMessage("Hello, gRPC!").build()
         with(responseObserver) {
             onNext(reply)
@@ -83,4 +135,25 @@ class ExampleEndpoint(private val dslContext: DSLContext) : ExampleServiceGrpc.E
         }
         log.info("Request processed")
     }
+}
+
+private fun createProducer(brokers: String): Producer<String, String> {
+    val props = Properties()
+            .apply {
+                this["bootstrap.servers"] = brokers
+                this["key.serializer"] = StringSerializer::class.java.canonicalName
+                this["value.serializer"] = StringSerializer::class.java.canonicalName
+            }
+    return KafkaProducer(props)
+}
+
+fun createConsumer(brokers: String): Consumer<String, String> {
+    val props = Properties()
+            .apply {
+                this["bootstrap.servers"] = brokers
+                this["group.id"] = "hello_world"
+                this["key.deserializer"] = StringDeserializer::class.java.canonicalName
+                this["value.deserializer"] = StringDeserializer::class.java.canonicalName
+            }
+    return KafkaConsumer(props)
 }
