494342185791c27456e45b7b1b07fb39e7d3e07c
Pavel Bukhmatov <buhmatov@gmail.com>
2020-10-07 16:32:41 +0300

    [kt] add ex handling examples

--- code-sample-kotlin/coroutines/src/main/kotlin/06_exHandlingAndPropagation.kt
+++ code-sample-kotlin/coroutines/src/main/kotlin/06_exHandlingAndPropagation.kt
@@ -2,7 +2,17 @@ import kotlinx.coroutines.*
 
 
 fun main() = runBlocking {
-
+    val handler = CoroutineExceptionHandler { _, exception ->
+        println("CoroutineExceptionHandler got $exception")
+    }
+    supervisorScope {
+        val child = launch(handler) {
+            println("The child throws an exception")
+            throw AssertionError()
+        }
+        println("The scope is completing")
+    }
+    println("The scope is completed")
 }
 
 fun mainExHandling2() = runBlocking {
