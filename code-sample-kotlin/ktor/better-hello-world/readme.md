d3c4754ae0fa32cdbd47b85f796924b6e89c60d4
Pavel Bukhmatov <buhmatov@gmail.com>
2020-10-10 13:48:52 +0300

    add request interceptor example

--- code-sample-kotlin/ktor/better-hello-world/readme.md
+++ code-sample-kotlin/ktor/better-hello-world/readme.md
@@ -10,7 +10,8 @@ Trying to implement complete production-ready project template with ktor and cor
 - **DONE** post requests (works with kotlinx serialization)
 - url (path) parameters
 - query (`?a=bla`) parameters
-- headers (generally - filters)
+- **DONE** headers
+- filters (time takes for request?)
 - log time taken for request processing (can do with custom coroutine scope key)
 - files
 - ktor http client 
@@ -19,12 +20,13 @@ Trying to implement complete production-ready project template with ktor and cor
 
 #### Logging
 
-- Make kotlinx serializations log much less? 
+- Make kotlinx serializations log much less (find where to configure)
 
 #### Testing
 
 - Check if starting up whole app does take too much time or not
 - Testcontainers (should be identical)
+- **DONE** Hot-realod
 
 #### Tools
 
@@ -35,6 +37,7 @@ Trying to implement complete production-ready project template with ktor and cor
     - Or this: https://medium.com/@OhadShai/async-with-style-kotlin-web-backend-with-ktor-coroutines-and-jasync-mysql-b34e8c83e4bd
     - or this: https://medium.com/@OhadShai/reactive-java-all-the-way-to-the-database-with-jasync-sql-and-javalin-c982365d7dd2
     - or this: https://medium.com/@OhadShai/just-a-small-example-of-how-kotlin-coroutines-are-great-c9774fe8434
+    - or even this: https://matej.laitl.cz/bench-rust-kotlin-microservices/
     - how micronaut / quarkus do it?
 - **Kafka**. Should be simple. How micronaut / quarkus do it?
 - **Consul**
--- code-sample-kotlin/ktor/better-hello-world/src/main/kotlin/ktor/Application.kt
+++ code-sample-kotlin/ktor/better-hello-world/src/main/kotlin/ktor/Application.kt
@@ -7,10 +7,9 @@ import io.ktor.http.*
 import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.serialization.*
+import io.ktor.util.*
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
-import kotlinx.serialization.serializer
 import org.slf4j.event.Level
 
 @Serializable
@@ -21,6 +20,11 @@ data class RequestData(val hello: String)
 
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+object MyAttributeKeys {
+    val strKey = AttributeKey<String>("atrkey")
+    val mapKey = AttributeKey<Map<String, String>>("atrmap")
+}
 
 fun Application.module(testing: Boolean = false) {
 
@@ -40,15 +44,25 @@ fun Application.module(testing: Boolean = false) {
         )
     }
 
+    intercept(ApplicationCallPipeline.Call) {
+        log.info("pipeline call interceptor executing")
+        call.attributes.put(MyAttributeKeys.strKey, "atrValue")
+        call.attributes.put(MyAttributeKeys.mapKey, mapOf("key" to "value"))
+    }
+
     routing {
         get("/") {
             val reqHeader = call.request.header("X-Other-Header")
-            val hello = HelloWorld("Hello, ", "world!")
+            val hello = HelloWorld("Hello, ", "world!!!")
+
             call.response.header("X-My-Header", "My-value")
             call.respond(HttpStatusCode.OK, hello)
         }
 
         get("/map") {
+
+            log.info("att str key: '${call.attributes.get(MyAttributeKeys.strKey)}'")
+            log.info("att map key: '${call.attributes.get(MyAttributeKeys.mapKey)}'")
             call.respond(mapOf("key" to "value"))
         }
 
