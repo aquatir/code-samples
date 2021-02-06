e44131cf11a442f22f7505d1fc98b3b297ded1a3
pavel bukhmatov <buhmatov@gmail.com>
2021-02-06 16:53:24 +0300

    Add resizing array impls

--- code-sample-kotlin/algorithms/src/main/kotlin/com/codesample/datastructs/basic/stackOnArray.kt
+++ code-sample-kotlin/algorithms/src/main/kotlin/com/codesample/datastructs/basic/stackOnArray.kt
@@ -27,11 +27,58 @@ class StackOnNonResizingArray<T>(private val maxSize: Int) : Stack<T> {
     }
 
     override fun toString(): String {
-        return "StackOnNonResizingArray(maxSize=$maxSize,curSize=$curSize,values=[${array.filterNotNull().joinToString(",")}])"
+        return "StackOnNonResizingArray(maxSize=$maxSize,curSize=$curSize,values=[${
+            array.filterNotNull().joinToString(",")
+        }])"
     }
 }
 
 
-class StackOnResizingArray<out T>() {
+/** Stack which uses an array and which will double in size when required and shrink by half when only 1/3 element
+ * are available */
+class StackOnResizingArray<T>() : Stack<T> {
+
+    // initial size in 2
+    private var array: MutableList<T?> = MutableList(2) { null }
+    private var curSize: Int = 0
+    private var curMaxSize: Int = 2
+
+
+    override fun push(value: T) {
+        if (curSize < curMaxSize) {
+            array[curSize] = value
+            curSize++
+        } else {
+            val toBeMaxSize = curMaxSize * 2
+            val newArray = MutableList(toBeMaxSize) { index -> if (index < curMaxSize) array[index] else null }
+            array = newArray
+            curMaxSize = toBeMaxSize
+            push(value)
+        }
+    }
+
+    override fun pop(): T {
+        return if (curSize == 0) throw RuntimeException("Stack is empty")
+        else {
+            val elem = array[curSize - 1]
+            array[curSize - 1] = null
+            curSize--
+
+            if (curSize != 0 && curMaxSize / curSize >= 3) { // if current max is at least 3 times larger them curSize -> shrink array in half
+                val toBeMaxSize = curMaxSize / 2
+                val newArray = MutableList(toBeMaxSize) { index -> array[index] }
+                array = newArray
+                curMaxSize = toBeMaxSize
+            }
+
+            elem!!
+        }
+    }
+
+    override fun toString(): String {
+        return "StackOnResizingArray(curSize=$curSize,curMaxSize=${curMaxSize},values=[${
+            array.filterNotNull().joinToString(",")
+        }])"
+    }
 
 }
---
+++ code-sample-kotlin/algorithms/src/test/kotlin/com/codesample/algo/StackOnResizingArrayTests.kt
@@ -0,0 +1,111 @@
+package com.codesample.algo
+
+import com.codesample.datastructs.basic.StackOnResizingArray
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class StackOnResizingArrayTests {
+
+    @Test
+    fun testStackInsert() {
+        val stack = StackOnResizingArray<Int>()
+            .also { it.push(1) }
+
+        Assertions.assertEquals("StackOnResizingArray(curSize=1,curMaxSize=2,values=[1])", stack.toString())
+    }
+
+    @Test
+    fun testStackInsertThree() {
+        val stack = StackOnResizingArray<Int>()
+            .also {
+                it.push(1)
+                it.push(2)
+                it.push(3)
+            }
+
+        Assertions.assertEquals("StackOnResizingArray(curSize=3,curMaxSize=4,values=[1,2,3])", stack.toString())
+    }
+
+    @Test
+    fun testStackInsertAndPop() {
+        val stack = StackOnResizingArray<Int>()
+            .also {
+                it.push(1)
+            }
+
+        val popped = stack.pop()
+
+        Assertions.assertEquals(1, popped)
+        Assertions.assertEquals("StackOnResizingArray(curSize=0,curMaxSize=2,values=[])", stack.toString())
+    }
+
+    @Test
+    fun testStackInsertTwiceAndPopTwice() {
+        val stack = StackOnResizingArray<Int>()
+            .also {
+                it.push(1)
+                it.push(2)
+            }
+
+        val popped1 = stack.pop()
+        val popped2 = stack.pop()
+
+        Assertions.assertEquals(2, popped1)
+        Assertions.assertEquals(1, popped2)
+        Assertions.assertEquals("StackOnResizingArray(curSize=0,curMaxSize=2,values=[])", stack.toString())
+    }
+
+    @Test
+    fun testPopOnEmptyThrowException() {
+        val stack = StackOnResizingArray<Int>()
+            .also {
+                it.push(1)
+                it.push(2)
+            }
+
+        val popped1 = stack.pop()
+        val popped2 = stack.pop()
+
+        Assertions.assertEquals(2, popped1)
+        Assertions.assertEquals(1, popped2)
+
+        assertThrows<RuntimeException> { stack.pop() }
+    }
+
+    @Test
+    fun testPushOverLimitResizesAnArray() {
+        val stack = StackOnResizingArray<Int>()
+            .also {
+                it.push(1)
+                it.push(2)
+            }
+
+        Assertions.assertEquals("StackOnResizingArray(curSize=2,curMaxSize=2,values=[1,2])", stack.toString())
+
+        stack.push(3)
+        Assertions.assertEquals("StackOnResizingArray(curSize=3,curMaxSize=4,values=[1,2,3])", stack.toString())
+
+    }
+
+    @Test
+    fun testPoppingTooMuchElementResizesAnArray() {
+        val stack = StackOnResizingArray<Int>()
+            .also {
+                it.push(1)
+                it.push(2)
+                it.push(3)
+                it.push(4)
+                it.push(5)
+            }
+
+        Assertions.assertEquals("StackOnResizingArray(curSize=5,curMaxSize=8,values=[1,2,3,4,5])", stack.toString())
+
+        stack.pop()
+        stack.pop()
+        stack.pop()
+        Assertions.assertEquals("StackOnResizingArray(curSize=2,curMaxSize=4,values=[1,2])", stack.toString())
+
+    }
+
+}
