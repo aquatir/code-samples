0e34b514870e6aff53c7340f392f4feb5af10842
pavel bukhmatov <buhmatov@gmail.com>
2021-02-07 12:15:13 +0300

    Implement queue on doubly linked list

--- code-sample-kotlin/algorithms/src/main/kotlin/com/codesample/datastructs/basic/queues.kt
+++ code-sample-kotlin/algorithms/src/main/kotlin/com/codesample/datastructs/basic/queues.kt
@@ -1,4 +1,73 @@
+import com.codesample.datastructs.basic.Node
+import java.lang.RuntimeException
+import java.lang.StringBuilder
 
-fun main() {
-    println("Hello, world!")
+interface Queue<T> {
+    fun enqueue(value: T)
+    fun dequeue(): T
+}
+
+class QueueOnDoublyLinkedList<T> : Queue<T> {
+
+    var head: Node<T>? = null
+    var tail: Node<T>? = null
+
+    override fun enqueue(value: T) {
+        if (tail == null) {
+            tail = Node(value, null)
+            head = tail
+        } else {
+            val newTail = Node(value, null)
+            tail!!.next = newTail
+            tail = newTail
+        }
+    }
+
+    override fun dequeue(): T {
+        return if (head == null) throw RuntimeException("No elements to dequeue")
+        else {
+            val oldHead = head
+            val value = head!!.value
+            head = head!!.next
+            oldHead!!.next = null
+
+            value
+        }
+    }
+
+    override fun toString(): String {
+        return when (head) {
+            null -> "QueueOnDoublyLinkedList(head=null,tail=null,values=[])"
+            tail -> "QueueOnDoublyLinkedList(head=${head!!.value},tail=${head!!.value},values=[${head!!.value}])"
+            else -> {
+                val strBuilder = StringBuilder()
+                strBuilder.append("QueueOnDoublyLinkedList(head=${head!!.value},tail=${tail!!.value},values=[")
+                var current = head
+                while (current?.next != null) {
+                    strBuilder.append("${current.value},")
+                    current = current.next
+                }
+                strBuilder.append("${tail!!.value}])")
+                strBuilder.toString()
+            }
+        }
+    }
+
+}
+
+class QueueOnResizingArray<T> : Queue<T> {
+
+    private var array: MutableList<T?> = MutableList(2) { null }
+    private var curSize: Int = 0
+    private var curMaxSize: Int = 2
+
+
+    override fun enqueue(value: T) {
+        TODO("Not yet implemented")
+    }
+
+    override fun dequeue(): T {
+        TODO("Not yet implemented")
+    }
+
 }
---
+++ code-sample-kotlin/algorithms/src/test/kotlin/com/codesample/algo/QueueOnDoublyLinkedListTests.kt
@@ -0,0 +1,86 @@
+package com.codesample.algo
+
+import QueueOnDoublyLinkedList
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import java.lang.RuntimeException
+
+class QueueOnDoublyLinkedListTests {
+
+    @Test
+    fun emptyQueue() {
+        val queue = QueueOnDoublyLinkedList<Int>()
+
+        assertEquals("QueueOnDoublyLinkedList(head=null,tail=null,values=[])", queue.toString())
+    }
+
+    @Test
+    fun enqueueOneElement() {
+        val queue = QueueOnDoublyLinkedList<Int>()
+            .also { it.enqueue(1) }
+
+        assertEquals("QueueOnDoublyLinkedList(head=1,tail=1,values=[1])", queue.toString())
+    }
+
+    @Test
+    fun enqueueTwoElement() {
+        val queue = QueueOnDoublyLinkedList<Int>()
+            .also {
+                it.enqueue(1)
+                it.enqueue(2)
+            }
+
+        assertEquals("QueueOnDoublyLinkedList(head=1,tail=2,values=[1,2])", queue.toString())
+    }
+
+    @Test
+    fun enqueueThreeElement() {
+        val queue = QueueOnDoublyLinkedList<Int>()
+            .also {
+                it.enqueue(1)
+                it.enqueue(2)
+                it.enqueue(3)
+            }
+
+        assertEquals("QueueOnDoublyLinkedList(head=1,tail=3,values=[1,2,3])", queue.toString())
+    }
+
+    @Test
+    fun enqueueOneElementAndDeque() {
+        val queue = QueueOnDoublyLinkedList<Int>()
+            .also { it.enqueue(1) }
+
+        assertEquals("QueueOnDoublyLinkedList(head=1,tail=1,values=[1])", queue.toString())
+
+        val elem1 = queue.dequeue()
+        assertEquals(1, elem1)
+
+        assertEquals("QueueOnDoublyLinkedList(head=null,tail=null,values=[])", queue.toString())
+    }
+
+    @Test
+    fun enqueueTwoElementsAndDequeTwo() {
+        val queue = QueueOnDoublyLinkedList<Int>()
+            .also {
+                it.enqueue(1)
+                it.enqueue(2)
+            }
+
+        assertEquals("QueueOnDoublyLinkedList(head=1,tail=2,values=[1,2])", queue.toString())
+
+        val elem1 = queue.dequeue()
+        val elem2 = queue.dequeue()
+        assertEquals(1, elem1)
+        assertEquals(2, elem2)
+
+        assertEquals("QueueOnDoublyLinkedList(head=null,tail=null,values=[])", queue.toString())
+    }
+
+    @Test
+    fun dequeOnEmptyThrows() {
+        val queue = QueueOnDoublyLinkedList<Int>()
+
+        assertThrows<RuntimeException> { queue.dequeue() }
+    }
+}
