ba525bbdcff4c1982e2a136d251aba6a5f7c8bac
pavel bukhmatov <buhmatov@gmail.com>
2021-06-25 16:55:11 +0300

    add cabal example

---
+++ code-sample-haskell/hid-cabal/app/Main.hs
@@ -0,0 +1,18 @@
+module Main where
+
+import Data.Char
+import Data.List (group, sort)
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+import System.Environment
+
+-- import Lib
+
+main :: IO ()
+main = do
+  [fname] <- getArgs
+  text <- TIO.readFile fname
+  let ws = map head $ group $ sort $ map T.toCaseFold $ filter (not . T.null)
+           $ map (T.dropAround $ not . isLetter) $ T.words text
+  TIO.putStrLn $ T.unwords ws
+  print $ length ws
