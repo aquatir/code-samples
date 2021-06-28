3fa8efdedd920d5e38a5a88b40b00df4859de62a
pavel bukhmatov <buhmatov@gmail.com>
2021-06-28 17:38:28 +0300

    [hs]: some more haskell

--- code-sample-haskell/hid-cabal/app/Radar.hs
+++ code-sample-haskell/hid-cabal/app/Radar.hs
@@ -1,22 +1,59 @@
 {-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE StandaloneDeriving #-}
+
+import Fmt
 
 class (Eq a, Enum a, Bounded a) => CyclicEnum a where
-  cpred :: a -> a
-  cpred d
-    | d == minBound = maxBound
-    | otherwise = pred d
-  csucc :: a -> a
-  csucc d
-    | d == maxBound = minBound
-    | otherwise = succ d
+    cpred :: a -> a
+    cpred d
+        | d == minBound = maxBound
+        | otherwise = pred d
+    csucc :: a -> a
+    csucc d
+        | d == maxBound = minBound
+        | otherwise = succ d
+
+-- 
 
 data Direction = North | East | South | West
     deriving (Eq, Enum, CyclicEnum, Bounded, Show)
--- instance CyclicEnum Direction
+deriving instance Read Direction
 
+instance Buildable Direction where
+    build North = "N"
+    build East = "E"
+    build South = "S"
+    build West = "W"
+
+
+-- 
 
 data Turn = TNone | TLeft | TRight | TAround
     deriving (Eq, Enum, Bounded, Show)
+deriving instance Read Turn
+deriving instance Ord Turn
+
+instance Buildable Turn where
+    build TNone = "--"
+    build TLeft = "<-"
+    build TRight = "->"
+    build TAround = "||"
+
+instance Semigroup Turn where
+    TNone <> t = t
+    TLeft <> TLeft = TAround
+    TLeft <> TRight = TNone
+    TLeft <> TAround = TRight
+    TRight <> TRight = TAround
+    TRight <> TAround = TLeft
+    TAround <> TAround = TNone
+    t1 <> t2 = t2 <> t1
+
+instance Monoid Turn where
+    mempty = TNone
+
+-- 
 
 rotate :: Turn -> Direction -> Direction
 rotate TNone = id
@@ -32,6 +69,7 @@ orient d1 d2 = head $ filter (\t -> rotate t d1 == d2) every
 
 rotateMany :: Direction -> [Turn] -> Direction
 rotateMany = foldl (flip rotate)
+rotateMany' dir turns = rotate (mconcat turns) dir
 
 rotateManySteps :: Direction -> [Turn] -> [Direction]
 rotateManySteps = scanl (flip rotate)
@@ -40,8 +78,22 @@ orientMany :: [Direction] -> [Turn]
 orientMany ds@(_:_:_) = zipWith orient ds (tail ds)
 orientMany _ = []
 
--- rotateFromFile :: Direction -> FilePath -> IO ()
+rotateFromFile :: Direction -> FilePath -> IO ()
+rotateFromFile dir fname = do
+    f <- readFile fname
+    let turns = map read $ lines f
+        finalDir = rotateMany dir turns
+        dirs = rotateManySteps dir turns
+    fmtLn $ "Final direction: "+||finalDir||+""
+    fmt $ nameF "Intermediate directions" (unwordsF dirs)
+   
 -- orientFromFile :: FilePath -> IO ()
-main :: IO ()
 
-main = print $ orientMany [North, South, South, North]
+
+main :: IO ()
+main = do
+    print $ orientMany [North, South, South, North]
+    print $ mconcat [TLeft, TRight, TAround, TAround]
+    print (read "North" :: Direction)
+    rotateFromFile South "examples/turns.txt"
+    --print $ orientFromFile "examples/dirs.txt"
