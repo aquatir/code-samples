ac3d50389f98c7bac1270387be1c482b6b0e552f
pavel bukhmatov <buhmatov@gmail.com>
2021-11-16 22:15:57 +0300

    add 338 and 392 leetcode

---
+++ code-sample-kotlin/algorithms/src/main/java/com/codesample/leetcode/easy/CountingBits_338.java
@@ -0,0 +1,49 @@
+package com.codesample.leetcode.easy;
+
+/**
+ * 338. Counting Bits — https://leetcode.com/problems/counting-bits/
+ * Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the
+ * number of 1's in the binary representation of i.
+ */
+public class CountingBits_338 {
+    static class Solution {
+
+        // 0    0      0
+        // 1    1      1
+        // 2    10     1
+        // 3    11     2
+        // 4    100    1
+        // 5    101    2
+        // 6    110    2
+        // 7    111    3
+        // 8    1000   1
+        // 9    1001   2
+        // 10   1010   2
+        // 11   1011   3
+        // 12   1100   2
+        // 13   1101   3
+        // 14   1110   3
+        // 15   1111   4
+        // 16   10000  1
+        // 17   10001  2
+
+        public int[] countBits(int n) {
+            int[] result = new int[n + 1];
+
+            result[0] = 0;
+
+            String kek = "";
+            kek.length();
+
+            for (int i = 1; i < n + 1; i++) {
+                if ((i - 1) % 2 == 0) {
+                    result[i] = result[i-1] + 1;
+                } else {
+                    result[i] = result[i/2];
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ code-sample-kotlin/algorithms/src/main/java/com/codesample/leetcode/easy/IsSubsequence_392.java
@@ -0,0 +1,46 @@
+package com.codesample.leetcode.easy;
+
+public class IsSubsequence_392 {
+    static class Solution {
+        public boolean isSubsequence(String s, String t) {
+            int si = 0;
+            int ti = 0;
+
+            if (s.isEmpty()) {
+                return true;
+            }
+
+            // abc
+            // ahbgdc
+
+            // 1: si = 0, ti = 0 => a == a => 0 != 2 =>
+            // 2: si = 1, ti = 1 => b != h =>
+            // 3: si = 1, ti = 2 => b == b => 1 != 2 =>
+            // 4: si = 2, ti = 3 => c != g =>
+            // 5: si = 2, ti = 4 => c != d =>
+            // 6: si = 2, ti = 5 => c == c => 2 == s => exit with true;
+
+            while( si <= s.length() - 1 && ti <= t.length() - 1) {
+
+                if (s.charAt(si) == t.charAt(ti)) {
+                    si++;
+                    ti++;
+
+                    if (si == s.length()) {
+                        return true;
+                    }
+
+                } else {
+                    ti++;
+                }
+            }
+
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        final var s = new Solution();
+        System.out.println(s.isSubsequence("abc", "ahbgdc"));
+    }
+}
