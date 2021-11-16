ab783d31c844d179e036ea5ce0b1f30a4eba3780
pavel bukhmatov <buhmatov@gmail.com>
2021-11-16 10:38:13 +0300

    Add smarter solution for max lenght of pos subarray

--- code-sample-kotlin/algorithms/src/main/java/com/codesample/leetcode/medium/MaximumLengthOfSubarrayWithPositiveProduct_1567.java
+++ code-sample-kotlin/algorithms/src/main/java/com/codesample/leetcode/medium/MaximumLengthOfSubarrayWithPositiveProduct_1567.java
@@ -4,7 +4,29 @@ public class MaximumLengthOfSubarrayWithPositiveProduct_1567 {
     static class Solution {
 
         public int getMaxLen(int[] nums) {
-            throw new RuntimeException("Not implemented yet");
+            int max = 0;
+            int positive = 0;
+            int negative = 0;
+
+            for (int i = 0; i < nums.length; i++) {
+                int num = nums[i];
+
+                if (num == 0) {
+                    positive = 0;
+                    negative = 0;
+                } else if (num > 0) {
+                    positive++;
+                    negative = negative == 0 ? 0 : negative + 1;
+                } else { // num < 0
+                    int tmpPos = positive;
+                    positive = negative == 0 ? 0 : negative + 1;
+                    negative = tmpPos + 1;
+                }
+
+                max = Math.max(max, positive);
+            }
+
+            return max;
         }
 
         // left to right, then right to left
@@ -26,7 +48,7 @@ public class MaximumLengthOfSubarrayWithPositiveProduct_1567 {
                     positives = 0;
                     negatives = 0;
                     inNegArray = false;
-                } else if ( num > 0) {
+                } else if (num > 0) {
                     if (inNegArray) {
                         negatives++;
                     } else {
@@ -65,7 +87,7 @@ public class MaximumLengthOfSubarrayWithPositiveProduct_1567 {
                     positives = 0;
                     negatives = 0;
                     inNegArray = false;
-                } else if ( num > 0) {
+                } else if (num > 0) {
                     if (inNegArray) {
                         negatives++;
                     } else {
@@ -100,15 +122,15 @@ public class MaximumLengthOfSubarrayWithPositiveProduct_1567 {
     public static void main(String[] args) {
         final var sol = new Solution();
 
-        System.out.println(sol.getMaxLen(new int[] {5,-20,-20,-39,-5,0,0,0,36,-32,0,-7,-10,-7,21,20,-12,-34,26,2})); // expected = 8 starting from -10
-        System.out.println(sol.getMaxLen(new int[] {1,2,3,5,-6,4,0,10})); // expected = 4
-        System.out.println(sol.getMaxLen(new int[] {-16,0,-5,2,2,-13,11,8})); // expected 6
-        System.out.println(sol.getMaxLen(new int[] {-1,-2,-3,0,1})); // expected = 2;
+        System.out.println(sol.getMaxLen(new int[]{5, -20, -20, -39, -5, 0, 0, 0, 36, -32, 0, -7, -10, -7, 21, 20, -12, -34, 26, 2})); // expected = 8 starting from -10
+        System.out.println(sol.getMaxLen(new int[]{1, 2, 3, 5, -6, 4, 0, 10})); // expected = 4
+        System.out.println(sol.getMaxLen(new int[]{-16, 0, -5, 2, 2, -13, 11, 8})); // expected 6
+        System.out.println(sol.getMaxLen(new int[]{-1, -2, -3, 0, 1})); // expected = 2;
 
-        System.out.println(sol.getMaxLenTwoPass(new int[] {5,-20,-20,-39,-5,0,0,0,36,-32,0,-7,-10,-7,21,20,-12,-34,26,2})); // expected = 8 starting from -10
-        System.out.println(sol.getMaxLenTwoPass(new int[] {1,2,3,5,-6,4,0,10})); // expected = 4
-        System.out.println(sol.getMaxLenTwoPass(new int[] {-16,0,-5,2,2,-13,11,8})); // expected 6
-        System.out.println(sol.getMaxLenTwoPass(new int[] {-1,-2,-3,0,1})); // expected = 2;
+        System.out.println(sol.getMaxLenTwoPass(new int[]{5, -20, -20, -39, -5, 0, 0, 0, 36, -32, 0, -7, -10, -7, 21, 20, -12, -34, 26, 2})); // expected = 8 starting from -10
+        System.out.println(sol.getMaxLenTwoPass(new int[]{1, 2, 3, 5, -6, 4, 0, 10})); // expected = 4
+        System.out.println(sol.getMaxLenTwoPass(new int[]{-16, 0, -5, 2, 2, -13, 11, 8})); // expected 6
+        System.out.println(sol.getMaxLenTwoPass(new int[]{-1, -2, -3, 0, 1})); // expected = 2;
 
         // at the end of -7,-10,-7,21,20,-12,-34,26,2
         // inNegArray = true
