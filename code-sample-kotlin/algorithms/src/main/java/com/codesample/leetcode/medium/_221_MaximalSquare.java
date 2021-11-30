b28eadf23e4cf5bec235586b63e6fd889100e46f
pavel bukhmatov <buhmatov@gmail.com>
2021-11-30 09:28:18 +0300

    add 221 max square fixes

--- code-sample-kotlin/algorithms/src/main/java/com/codesample/leetcode/medium/_221_MaximalSquare.java
+++ code-sample-kotlin/algorithms/src/main/java/com/codesample/leetcode/medium/_221_MaximalSquare.java
@@ -20,18 +20,18 @@ public class _221_MaximalSquare {
             int n = matrix[0].length;
             int max = 0;
 
-            int[][] b = new int[m + 1][n + 1];
+            int[][] dp = new int[m + 1][n + 1];
             for (int i = 1; i <= m; i++) {
                 for (int j = 1; j <= n; j++) {
                     if (matrix[i - 1][j - 1] == '1') {
-                        b[i][j] = Math.min(
+                        dp[i][j] = Math.min(
                                 Math.min(
-                                        b[i][j - 1],
-                                        b[i - 1][j - 1]
+                                        dp[i][j - 1],
+                                        dp[i - 1][j]
                                 ),
-                                b[i - 1][j]
+                                dp[i - 1][j - 1]
                         ) + 1;
-                        max = Math.max(b[i][j], max);
+                        max = Math.max(dp[i][j], max);
                     }
                 }
             }
