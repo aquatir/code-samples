5d425537bb9dbf2572b3c8bf2db626c8eb88b241
pavel bukhmatov <buhmatov@gmail.com>
2022-03-12 17:32:55 +0300

    add py conditions

--- code-sample-python/pythonProject/02_conditions.py
+++ code-sample-python/pythonProject/02_conditions.py
@@ -1,2 +1,16 @@
+import random
+
 if __name__ == '__main__':
-    print("kekw")
+
+    a = random.randint(0, 10)
+    print(f"a: {a}")
+    if a > 5:
+        print("more than 5")
+    elif a == 5:
+        print("equal to 5")
+    else:
+        print("less than 5")
+
+    # elvis operator but other way around
+    b = "more than 5" if a > 5 else "less or equal to 5"
+    print(b)
