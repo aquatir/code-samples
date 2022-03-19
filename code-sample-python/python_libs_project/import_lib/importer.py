ac9a8d236e021e76b6bc77e6754431a2fe4f2fdc
pavel bukhmatov <buhmatov@gmail.com>
2022-03-19 18:04:08 +0300

    manual imports

---
+++ code-sample-python/python_libs_project/import_lib/importer.py
@@ -0,0 +1,15 @@
+import importlib
+import foo
+
+
+def dynamic_import(module):
+
+    return importlib.import_module(module)
+
+
+if __name__ == '__main__':
+    module = dynamic_import('foo')
+    module.main()
+
+    module_two = dynamic_import('bar')
+    module_two.main()
