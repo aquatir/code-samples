dca2fb785a6643142736276b30408e30ec07b81f
pavel bukhmatov <buhmatov@gmail.com>
2022-03-24 13:35:08 +0300

    add validation for models + migrations

--- code-sample-python/python_second_django/app/admin.py
+++ code-sample-python/python_second_django/app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from app.models import Post, Topic, Webpage, AccessRecord
+
+admin.site.register(Post)
+admin.site.register(Topic)
+admin.site.register(Webpage)
+admin.site.register(AccessRecord)
--- code-sample-python/python_second_django/app/models.py
+++ code-sample-python/python_second_django/app/models.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
@@ -9,3 +11,28 @@ class Post(models.Model):
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
+
+
+class Topic(models.Model):
+    top_name = models.CharField(max_length=264, unique=True)
+
+    def __str__(self):
+        return self.top_name
+
+
+class Webpage(models.Model):
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    name = models.CharField(max_length=264, unique=True)
+    url = models.URLField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class AccessRecord(models.Model):
+    name = models.ForeignKey(Webpage, on_delete=models.CASCADE, help_text="the name of web page")
+    date = models.DateField(default=timezone.now)
+    count = models.IntegerField(default=0)
+
+    def __str__(self):
+        return str(self.date)
