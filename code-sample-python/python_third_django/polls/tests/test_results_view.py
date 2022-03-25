5b079f4eab5e78277cd6aab77a82f4c063b70862
pavel bukhmatov <buhmatov@gmail.com>
2022-03-26 01:55:55 +0300

    add more tests

---
+++ code-sample-python/python_third_django/polls/tests/test_results_view.py
@@ -0,0 +1,32 @@
+from django.test import TestCase
+from django.urls import reverse
+
+from polls.models import Choice
+from polls.tests.test_utils import create_question
+
+
+class ResultsViewTests(TestCase):
+
+    def test_future_question(self):
+        """
+        Results can be queried
+        """
+        future_question = create_question(question_text='question.', days=0)
+        url = reverse('polls:results', args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_all_choices_are_shown(self):
+        """All possible choices and their results must be shown"""
+        question = create_question(question_text="test_question", days=0)
+        question.choice_set.create(choice_text="test_choice_1", votes=1)
+        question.choice_set.create(choice_text="test_choice_2", votes=2)
+
+        url = reverse('polls:results', args=(question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "test_question")
+        self.assertContains(response, "test_choice_1")
+        self.assertContains(response, "1 vote")
+        self.assertContains(response, "test_choice_2")
+        self.assertContains(response, "2 votes")
