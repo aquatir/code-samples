d837dff25100b33f5deb15dbed889ba6916071f6
pavel bukhmatov <buhmatov@gmail.com>
2022-03-26 17:13:55 +0300

    Use SimpleTestCase when testing the model without saving

--- code-sample-python/python_third_django/polls/tests/test_question_model.py
+++ code-sample-python/python_third_django/polls/tests/test_question_model.py
@@ -1,13 +1,13 @@
 import datetime
 
-from django.test import TestCase
+from django.test import SimpleTestCase
 from django.utils import timezone
 
 from polls.models import Question
 
 
 # Create your tests here.
-class QuestionModelTests(TestCase):
+class QuestionModelTests(SimpleTestCase):
 
     def test_was_published_recently_with_future_question(self):
         """
--- code-sample-python/python_third_django/python_third_django/settings.py
+++ code-sample-python/python_third_django/python_third_django/settings.py
@@ -77,6 +77,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+
 }
 
 # Password validation
