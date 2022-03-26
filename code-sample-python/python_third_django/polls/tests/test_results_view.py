5ebc9a420c48eb7eaa041aa32a75da73fc9d80ee
pavel bukhmatov <buhmatov@gmail.com>
2022-03-26 12:51:57 +0300

    better test for results

--- code-sample-python/python_third_django/polls/tests/test_results_view.py
+++ code-sample-python/python_third_django/polls/tests/test_results_view.py
@@ -17,14 +17,26 @@ class ResultsViewTests(TestCase):
 
     def test_all_choices_are_shown(self):
         """All possible choices and their results must be shown"""
-        question = create_question(question_text="test_question", days=0)
-        question.choice_set.create(choice_text="test_choice_1", votes=1)
-        question.choice_set.create(choice_text="test_choice_2", votes=2)
 
+        # given
+        question = create_question(question_text="test_question", days=0)
+        choice_1 = question.choice_set.create(choice_text="test_choice_1", votes=1)
+        choice_2 = question.choice_set.create(choice_text="test_choice_2", votes=2)
+
+        # do
         url = reverse('polls:results', args=(question.id,))
         response = self.client.get(url)
+        question_response = response.context['question']
+        choices_response = question_response.choice_set.all()
+
+        # expect status is 200
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "test_question")
+
+        # expect the same values are returned
+        self.assertEqual(question_response, question)
+        self.assertListEqual(list(question.choice_set.all()), list(choices_response))
+
+        # expect template text contains correct values
         self.assertContains(response, "test_choice_1")
         self.assertContains(response, "1 vote")
         self.assertContains(response, "test_choice_2")
