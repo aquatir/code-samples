54360bda3d02bb97ffee66310666312ca3c78688
pavel bukhmatov <buhmatov@gmail.com>
2022-03-31 22:24:08 +0300

    add pytest exam

--- code-sample-python/python_libs_project/testing/test_some_math_pytest.py
+++ code-sample-python/python_libs_project/testing/test_some_math_pytest.py
@@ -1,30 +1,29 @@
 import sys
-
 import unittest
 
-
-# Run this class only: python3 testing/test_some_math.py
-# or python3 -m unittest test_some_math.py
 from . import some_math
 
 
-class TestAdd(unittest.TestCase):
+# Run with 'pytest'
+# You can mix both unittest tests and pytest tests
+
+class TestAdd:
     """
     Test the add function from the some_math library
     """
 
     @classmethod
-    def setUpClass(cls) -> None:
+    def set_up_class(cls) -> None:
         print("run before all tests")
 
-    def setUp(self):
+    def set_up(self):
         print("--run before each test")
 
-    def tearDown(self):
+    def tear_down(self):
         print("--run after each test")
 
     @classmethod
-    def tearDownClass(cls):
+    def tear_down_class(cls):
         print("run after all tests")
 
     def test_add_integers(self):
@@ -33,7 +32,7 @@ class TestAdd(unittest.TestCase):
         """
         print("---running test_add_integers")
         result = some_math.add(1, 2)
-        self.assertEqual(result, 3)
+        assert result == 3
 
     def test_add_floats(self):
         """
@@ -41,7 +40,7 @@ class TestAdd(unittest.TestCase):
         """
         print("---running test_add_floats")
         result = some_math.add(10.5, 2)
-        self.assertEqual(result, 12.5)
+        assert result == 12.5
 
     @unittest.skip('Skip this test')
     def test_add_strings(self):
@@ -51,44 +50,14 @@ class TestAdd(unittest.TestCase):
         """
         print("---running test_add_strings")
         result = some_math.add('abc', 'def')
-        self.assertEqual(result, 'abcdef')
+        assert result == 'abcdef'
 
     @unittest.skipUnless(sys.platform.startswith("win"), "requires Windows")
     def test_adding_on_windows(self):
         result = some_math.add(1, 2)
-        self.assertEqual(result, 3)
+        assert result == 3
 
     @unittest.skipUnless(sys.platform.startswith("darwin"), "requires darwin")
     def test_adding_on_darwin(self):
         result = some_math.add(1, 2)
-        self.assertEqual(result, 3)
-
-
-class TestMultiply(unittest.TestCase):
-    """
-    Test the multiply function from the mymath library
-    """
-
-    def test_subtract_integers(self):
-        """
-        Test that multiplying integers returns the correct result
-        """
-        result = some_math.multiply(5, 50)
-        self.assertEqual(result, 250)
-
-
-class TestDivide(unittest.TestCase):
-    """
-    Test the divide function from the mymath library
-    """
-
-    def test_divide_by_zero(self):
-        """
-        Test that multiplying integers returns the correct result
-        """
-        with self.assertRaises(ZeroDivisionError):
-            result = some_math.divide(8, 0)
-
-
-if __name__ == '__main__':
-    unittest.main()
+        assert result == 3
