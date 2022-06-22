795b96ccf95538b6d1b5ddf95d9b18f56e6dc794
pavel bukhmatov <buhmatov@gmail.com>
2022-06-22 11:08:57 +0400

    expose prometheus database metrics

--- code-sample-python/python_django_prometheus/python_django_prometheus/settings.py
+++ code-sample-python/python_django_prometheus/python_django_prometheus/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'python_django_prometheus.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django_prometheus.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
