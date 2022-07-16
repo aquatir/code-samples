6cd64ce93c2e0f3663a598b15d14a862e05c7898
pavel bukhmatov <buhmatov@gmail.com>
2022-07-16 09:35:09 +0400

    solve 07

--- code-sample-java/code-sample-java-algorithms-and-data-structures/src/main/java/codesample/other/educative/_04_merge_intervals/_07_EmployeeFreeTime.java
+++ code-sample-java/code-sample-java-algorithms-and-data-structures/src/main/java/codesample/other/educative/_04_merge_intervals/_07_EmployeeFreeTime.java
@@ -2,7 +2,6 @@ package codesample.other.educative._04_merge_intervals;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -11,21 +10,22 @@ import java.util.stream.Collectors;
  * For ‘K’ employees, we are given a list of intervals representing the working hours of each employee. Our goal is to
  * find out if there is a free interval that is common to all employees. You can assume that each list of employee
  * working hours is sorted on the start time.
- *
+ * <p>
  * Input: Employee Working Hours=[[[1,3], [5,6]], [[2,3], [6,8]]]
  * Output: [3,5]
  * Explanation: Both the employees are free between [3,5].
- *
+ * <p>
  * Input: Employee Working Hours=[[[1,3], [9,12]], [[2,4]], [[6,8]]]
  * Output: [4,6], [8,9]
  * Explanation: All employees are free between [4,6] and [8,9].
- *
+ * <p>
  * Input: Employee Working Hours=[[[1,3]], [[2,4]], [[3,5], [7,9]]]
  * Output: [5,7]
  * Explanation: All employees are free between [5,7].
  */
 public class _07_EmployeeFreeTime {
     public static List<Interval> findEmployeeFreeTime(List<List<Interval>> schedule) {
+
         /**
          * Free times in all lists => no overlap anywhere
          * 1. Add everything into a single list
@@ -33,17 +33,26 @@ public class _07_EmployeeFreeTime {
          * 3. find intervals with no overlap
          */
 
-        // TODO ???????
-        List<Interval> noOverlaps = schedule.stream().flatMap(List::stream).collect(Collectors.toList());
-        Collections.sort(noOverlaps, Comparator.comparing(interval -> interval.start));
+        List<Interval> noOverlaps = schedule.stream()
+            .flatMap(List::stream)
+            .sorted(Comparator.comparing(interval -> interval.start))
+            .collect(Collectors.toList());
+
+        List<Interval> result = new ArrayList<>();
 
         var first = noOverlaps.get(0);
         for (int i = 1; i < noOverlaps.size(); i++) {
             var second = noOverlaps.get(i);
 
+            // if overlap => make first bigger
+            if (first.end >= second.start) {
+                first.start = Math.min(first.start, second.start);
+                first.end = Math.max(first.end, second.end);
+            } else {
+                result.add(new Interval(first.end, second.start));
+                first = second;
+            }
         }
-
-        List<Interval> result = new ArrayList<>();
 
         return result;
     }
