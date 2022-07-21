7349d27f854958f01c4840c562008f61b1519b69
pavel bukhmatov <buhmatov@gmail.com>
2022-07-21 21:40:22 +0400

    add reverse alternating k elements but not full

--- code-sample-java/code-sample-java-algorithms-and-data-structures/src/main/java/codesample/other/educative/_06_in_place_linked_list_revert/_01_ReverseAlternatingKElementSubList.java
+++ code-sample-java/code-sample-java-algorithms-and-data-structures/src/main/java/codesample/other/educative/_06_in_place_linked_list_revert/_01_ReverseAlternatingKElementSubList.java
@@ -1,13 +1,11 @@
 package codesample.other.educative._06_in_place_linked_list_revert;
 
-import java.util.Map;
 import java.util.Optional;
-import java.util.function.Supplier;
 
 /**
  * Reverse alternating K-element Sub-list (medium)#
  * Given the head of a LinkedList and a number ‘k’, reverse every alternating ‘k’ sized sub-list starting from the head.
- *
+ * <p>
  * If, in the end, you are left with a sub-list with less than ‘k’ elements, reverse it too.
  */
 public class _01_ReverseAlternatingKElementSubList {
@@ -21,6 +19,48 @@ public class _01_ReverseAlternatingKElementSubList {
     }
 
     public static ListNode reverse(ListNode head, int k) {
+        // 1. learn to rotate k elements only
+        // 2. then learn to rotate sublists on k elements
+        // 3. skip 2 every other time
+
+        // 1
+
+
+        ListNode prev = null;
+        ListNode cur = head;
+
+        while (true) {
+
+            ListNode lastNodeOfPrev = prev;
+            ListNode lastNodeOfSublist = cur; // we know it because it's the first node we process
+
+            if (prev != null) {
+                System.out.println("lastNodeOfPrev: " + lastNodeOfPrev.value + ", lastNodeOfSublist: " + lastNodeOfSublist.value);
+            } else {
+                System.out.println("lastNodeOfPrev: null, lastNodeOfSublist: " + lastNodeOfSublist.value);
+            }
+            // rotate
+            for (int i = 0; i < k && cur != null; i++) {
+                ListNode next = cur.next;
+                cur.next = prev;
+                prev = cur;
+                cur = next;
+            }
+
+            // re-assign head
+            if (lastNodeOfPrev != null) {
+                System.out.println("lastNodeOfPrev.next: " + lastNodeOfPrev.next.value + " => " + prev.value);
+                lastNodeOfPrev.next = prev;
+            } else {
+                head = prev;
+            }
+            lastNodeOfSublist.next = cur;
+            prev = lastNodeOfSublist;
+
+            if (cur == null) {
+                break;
+            }
+        }
 
         return head;
     }
