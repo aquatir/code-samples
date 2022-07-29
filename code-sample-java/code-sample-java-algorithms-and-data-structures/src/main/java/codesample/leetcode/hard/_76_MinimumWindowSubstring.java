c41f34f67899fb91ebdaa697e345a6a24bc26448
pavel bukhmatov <buhmatov@gmail.com>
2022-07-29 08:49:57 +0400

    add comments to 76 minimum window substring

--- code-sample-java/code-sample-java-algorithms-and-data-structures/src/main/java/codesample/leetcode/hard/_76_MinimumWindowSubstring.java
+++ code-sample-java/code-sample-java-algorithms-and-data-structures/src/main/java/codesample/leetcode/hard/_76_MinimumWindowSubstring.java
@@ -5,6 +5,13 @@ import java.util.Map;
 
 /**
  * 76. Minimum Window Substring — https://leetcode.com/problems/minimum-window-substring/
+ *
+ * Given two strings s and t of lengths m and n respectively, return the minimum window substring of s such that every
+ * character in t (including duplicates) is included in the window. If there is no such substring, return the empty string "".
+ *
+ * The testcases will be generated such that the answer is unique.
+ *
+ * A substring is a contiguous sequence of characters within the string.
  */
 public class _76_MinimumWindowSubstring {
     public String minWindow(String s, String t) {
@@ -46,6 +53,7 @@ public class _76_MinimumWindowSubstring {
                     }
                     freqMap.put(charAtStart, freqMap.get(charAtStart) + 1);
                 }
+                
             }
         }
         return curMatchLength > s.length() ? "" : s.substring(subStrStart, subStrStart + curMatchLength);
