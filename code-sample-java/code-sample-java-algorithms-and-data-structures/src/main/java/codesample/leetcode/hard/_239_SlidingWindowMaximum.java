c70560b25a112ee31c4e4d645dd90d978965d857
pavel bukhmatov <buhmatov@gmail.com>
2022-08-04 09:37:25 +0400

    add 239 sliding window maximum

--- code-sample-java/code-sample-java-algorithms-and-data-structures/src/main/java/codesample/leetcode/hard/_239_SlidingWindowMaximum.java
+++ code-sample-java/code-sample-java-algorithms-and-data-structures/src/main/java/codesample/leetcode/hard/_239_SlidingWindowMaximum.java
@@ -1,43 +1,63 @@
 package codesample.leetcode.hard;
 
-import java.util.Comparator;
-import java.util.PriorityQueue;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 /**
  * 239. Sliding Window Maximum — https://leetcode.com/problems/sliding-window-maximum/
- *
+ * <p>
  * You are given an array of integers nums, there is a sliding window of size k which is moving from the very left of the array to the very right. You can only see the k numbers in the window. Each time the sliding window moves right by one position.
- *
+ * <p>
  * Return the max sliding window.
- *
+ * <p>
  * NOTE: NOT COMPLETE SOLUTION
  */
 public class _239_SlidingWindowMaximum {
 
-    // should work, but fails on time limit exceeded, because remove on PriorityQueue is slow.
+    // approach 1: with priority queue: should work, but fails on time limit exceeded, because remove on PriorityQueue is slow.
+    // approach 2: with deque.
     public int[] maxSlidingWindow(int[] nums, int k) {
 
+        if (nums.length == 0) {
+            return nums;
+        }
+
         int[] result = new int[nums.length - k + 1];
+        int resIndex = 0;
 
+        // put indexes of max elements into the deque, because we need index for size
+        Deque<Integer> deque = new ArrayDeque<>();
 
-        if (nums.length == 1) {
-            result[0] = nums[0];
-            return result;
+        for (int i = 0; i < nums.length; i++) {
+            // preserve deque size less than k.
+            // remove elements which were added first (e.g. normal queue behaviour)
+            while (!deque.isEmpty() && deque.peekFirst() < i - k + 1) {
+                deque.pollFirst();
+            }
+
+            // now, also remove element which were added last, e.g. in stack fashion.
+            // we remove everything which is definitely less than nums[i]
+            // after that the deque's leftmost element will be the max
+            // this is also true on each iteration.
+            while (!deque.isEmpty() && nums[deque.peekLast()] < nums[i]) {
+                deque.pollLast();
+            }
+
+            deque.offerLast(i);
+
+            // after while above the leftmost index is max, so we record int
+            if (i >= k - 1) {
+                result[resIndex++] = nums[deque.peekFirst()];
+            }
         }
-
-        PriorityQueue<Integer> maxQueue = new PriorityQueue<>(Comparator.reverseOrder());
-
-        for (int i = 0; i < k; i++) {
-            maxQueue.offer(nums[i]);
-        }
-        result[0] = maxQueue.peek();
-
-        for (int i = k; i < nums.length; i++) {
-            maxQueue.remove(nums[i - k]);
-            maxQueue.offer(nums[i]);
-            result[i - k + 1] = maxQueue.peek();
-        }
-
         return result;
+    }
+
+    public static void main(String[] args) {
+        var s = new _239_SlidingWindowMaximum();
+        var res1 = s.maxSlidingWindow(new int[]{1,3,-1,-3,5,3,6,7}, 3);
+        for (int r: res1) {
+            System.out.print(r + ", "); // expected [3,3,5,5,6,7]
+        }
     }
 }
