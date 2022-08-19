4158771916e2fe17bd32ab8aec8f1eab9d4f3c5f
pavel bukhmatov <buhmatov@gmail.com>
2022-08-19 07:39:47 +0400

    744 smallest letter greater than target

---
+++ code-sample-java/code-sample-java-algorithms-and-data-structures/src/main/java/codesample/leetcode/easy/_744_FindSmallestLetterGreaterThanTarget.java
@@ -0,0 +1,48 @@
+package codesample.leetcode.easy;
+
+/**
+ * 744. Find Smallest Letter Greater Than Target — https://leetcode.com/problems/find-smallest-letter-greater-than-target/
+ * <p>
+ * Given a characters array letters that is sorted in non-decreasing order and a character target, return the smallest character in the array that is larger than target.
+ * <p>
+ * Note that the letters wrap around.
+ * <p>
+ * For example, if target == 'z' and letters == ['a', 'b'], the answer is 'a'.
+ */
+public class _744_FindSmallestLetterGreaterThanTarget {
+    public char nextGreatestLetter(char[] letters, char target) {
+        var left = 0;
+        var right = letters.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right-left)/2;
+
+            char atMid = letters[mid];
+
+            if ( (target - atMid) > 0) {
+                left = mid + 1;
+            } else if ( (target - atMid) < 0) {
+                right = mid - 1;
+            } else {
+                while(letters[mid] == target) {
+                    mid++;
+                    if (mid == letters.length) {
+                        mid = 0;
+                    }
+                }
+                return letters[mid];
+            }
+        }
+
+        if (left == letters.length) {
+            return letters[0];
+        } else {
+            return letters[left];
+        }
+    }
+
+    public static void main(String[] args) {
+        var s = new _744_FindSmallestLetterGreaterThanTarget();
+        System.out.println(s.nextGreatestLetter(new char[]{'c', 'f', 'j'}, 'c')); // expected: f
+    }
+}
