aba2fce74a271c29822b85cb8050252f67a8cbde
pavel bukhmatov <buhmatov@gmail.com>
2023-01-05 11:02:00 +0400

    add 160 better

--- code-sample-java/code-sample-java-algorithms-and-data-structures/src/main/java/codesample/leetcode/easy/_160_IntersectionOfTwoLinkedLists.java
+++ code-sample-java/code-sample-java-algorithms-and-data-structures/src/main/java/codesample/leetcode/easy/_160_IntersectionOfTwoLinkedLists.java
@@ -22,59 +22,34 @@ public class _160_IntersectionOfTwoLinkedLists {
             //  iterate to find the loop point form the other one
             //  if not found => no intersection. If found => break the cycle are return the node
 
-            // Approach 2 (implemented): find lengths of both lists + iterate over them until the end
+            // Approach 2 (git history): find lengths of both lists + iterate over them until the end
             //  if the end is not the same for both lists => there is no intersection
             //      the length after interception is >= length of the shortest list
             //      find longer and shorter list
             //      iterate longer list until they both have the same size
             //      iterate both lists until we find an intersection point
 
+            // Approach 3 (implemented): iterate both lists at the same time. When one of the lists reaches the end
+            // reset it's head to another list's head => this is pretty much the same things as in Approach 2 where we
+            // calculate the length and move the pointer only here we don't "calculate" but simply move two heads
+            // because it's the same distance.
+            //
+            // Due to the fact that shorter list will hit the end faster, and we move it's head to a longer list, it
+            // will pass exactly a difference of longer minus shorted lengths before the other list switch too => this is
+            // the same length difference as in Approach 2.
+            //
+            // We than keep iterating before the intersection point OR before both lists hit null node again if there is
+            // no intersection
+
             var listOne = headA;
             var listTwo = headB;
 
-            var lengthOne = 1;
-            var lengthTwo = 1;
-
-            while (listOne.next != null) {
-                listOne = listOne.next;
-                lengthOne++;
+            while (listOne != listTwo) {
+                listOne = listOne == null ? headB : listOne.next;
+                listTwo = listTwo == null ? headA : listTwo.next;
             }
 
-            while (listTwo.next != null) {
-                listTwo = listTwo.next;
-                lengthTwo++;
-            }
-
-            // lists ends with different nodes => no intersection
-            if (listOne != listTwo) {
-                return null;
-            }
-
-            ListNode longerList, shorterList = null;
-            int longerSize, shorterSize = 0;
-
-            if (lengthOne >= lengthTwo) {
-                longerList = headA;
-                longerSize = lengthOne;
-                shorterList = headB;
-                shorterSize = lengthTwo;
-            } else {
-                longerList = headB;
-                longerSize = lengthTwo;
-                shorterList = headA;
-                shorterSize = lengthOne;
-            }
-
-            while (longerSize != shorterSize) {
-                longerList = longerList.next;
-                longerSize--;
-            }
-
-            while(longerList != shorterList) {
-                shorterList = shorterList.next;
-                longerList = longerList.next;
-            }
-            return longerList;
+            return listOne;
         }
     }
 }
