d78bd68c14e2fa522457dbf68c70986551b24521
pavel bukhmatov <buhmatov@gmail.com>
2023-04-04 09:57:59 +0400

    add recursive solution for 344 — reverse string

--- code-sample-java/code-sample-java-algorithms-and-data-structures/src/main/java/codesample/leetcode/easy/_344_ReverseString.java
+++ code-sample-java/code-sample-java-algorithms-and-data-structures/src/main/java/codesample/leetcode/easy/_344_ReverseString.java
@@ -4,16 +4,33 @@ package codesample.leetcode.easy;
  * 344. Reverse String — https://leetcode.com/problems/reverse-string/description/
  */
 public class _344_ReverseString {
-    public void reverseString(char[] s) {
-        int left = 0;
-        int right = s.length - 1;
 
-        while (left < right) {
-            var tmp = s[left];
-            s[left] = s[right];
-            s[right] = tmp;
-            left++;
-            right--;
+    // Iterative
+//    public void reverseString(char[] s) {
+//        int left = 0;
+//        int right = s.length - 1;
+//
+//        while (left < right) {
+//            var tmp = s[left];
+//            s[left] = s[right];
+//            s[right] = tmp;
+//            left++;
+//            right--;
+//        }
+//    }
+
+    // recursive
+    public void reverseString(char[] s) {
+        reverseString(s, 0, s.length - 1);
+    }
+
+    public void reverseString(char[] s, int from, int to) {
+        // base case: from >= to => do nothing & return
+        if (from < to) {
+            var tmp = s[from];
+            s[from] = s[to];
+            s[to] = tmp;
+            reverseString(s, from + 1, to - 1);
         }
     }
 }
