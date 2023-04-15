8e2d895b383b539061445860ae6d620fb7e68ccf
pavel bukhmatov <buhmatov@gmail.com>
2023-04-15 09:15:50 +0400

    add 700 search in binary tree

---
+++ code-sample-java/code-sample-java-algorithms-and-data-structures/src/main/java/codesample/leetcode/easy/_700_SearchInABinarySearchTree.java
@@ -0,0 +1,41 @@
+package codesample.leetcode.easy;
+
+/**
+ * 700. Search in a Binary Search Tree — https://leetcode.com/problems/search-in-a-binary-search-tree/description/
+ */
+public class _700_SearchInABinarySearchTree {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val == val) {
+            return root;
+        }
+        if (val > root.val) {
+            return searchBST(root.right, val);
+        } else {
+            return searchBST(root.left, val);
+        }
+    }
+}
+
