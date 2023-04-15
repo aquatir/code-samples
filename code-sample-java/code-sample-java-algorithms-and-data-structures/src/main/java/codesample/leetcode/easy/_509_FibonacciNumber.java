8aede867dcbfa10b79779e4667d66107556c7861
pavel bukhmatov <buhmatov@gmail.com>
2023-04-15 09:31:25 +0400

    Add 509 Fibonacci number

--- code-sample-java/code-sample-java-algorithms-and-data-structures/src/main/java/codesample/leetcode/easy/_509_FibonacciNumber.java
+++ code-sample-java/code-sample-java-algorithms-and-data-structures/src/main/java/codesample/leetcode/easy/_509_FibonacciNumber.java
@@ -6,20 +6,45 @@ package codesample.leetcode.easy;
  * is the sum of the two preceding ones, starting from 0 and 1. That is,
  */
 public class _509_FibonacciNumber {
+
+    // iterative, sort of DP
+//    public int fib(int n) {
+//        if (n <= 0) return 0;
+//        if (n <= 2) return 1;
+//        int veryPrev = 1;
+//        int prev = 1;
+//        int cur = 0;
+//        for (int i = 3; i <= n; i++) {
+//            cur = veryPrev + prev;
+//            veryPrev = prev;
+//            prev = cur;
+//        }
+//
+//        return cur;
+//    }
+
+    // Recursive
     public int fib(int n) {
-        if (n <= 0) return 0;
-        if (n <= 2) return 1;
-        int veryPrev = 1;
-        int prev = 1;
-        int cur = 0;
-        for (int i = 3; i <= n; i++) {
-            cur = veryPrev + prev;
-            veryPrev = prev;
-            prev = cur;
+
+        // +2 to handle (n=0) case
+        var memo = new int[n+2];
+        memo[0] = 0;
+        memo[1] = 1;
+
+        return fib(n, memo);
+    }
+
+    private int fib(int n, int[] memo) {
+        if (n == 0 || memo[n] != 0) {
+            return memo[n];
         }
 
-        return cur;
+        int res = fib(n - 1, memo) + fib(n - 2, memo);
+        memo[n] = res;
+
+        return res;
     }
+
 
     public static void main(String[] args) {
         final var fib = new _509_FibonacciNumber();
