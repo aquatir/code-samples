b3fbda11a58b3422557085f7d5af828d14fad10a
pavel bukhmatov <58176914+yandex-praktikum@users.noreply.github.com>
2023-05-30 09:16:08 +0400

    add 779

--- code-sample-java/code-sample-java-algorithms-and-data-structures/src/main/java/codesample/leetcode/medium/_779_KThSymbolInGrammar.java
+++ code-sample-java/code-sample-java-algorithms-and-data-structures/src/main/java/codesample/leetcode/medium/_779_KThSymbolInGrammar.java
@@ -7,18 +7,24 @@ public class _779_KThSymbolInGrammar {
     static class Solution {
         public int kthGrammar(int n, int k) {
 
-            // after applying the operation left half of array is exactly the same as right half
-            // we can use recursion to either find the element in left half or right half
+            // see the problem as a tree
+            //row1. |        0
+            //row2. |    0      1
+            //row3. |  0   1   1   1
+            //row4. | 0 1 1 0 1 0 0 1
+            // ------------------------
+            //index   1 2 3 4 5 6 7 8
+            // Here it becomes obvious that the parent of Kth index in Nth row is
+            // either k/2 index when (n-1) is even
+            // (k+1)/2 index when (n-1) is odd
             if (n == 1 && k == 1) {
                 return 0;
             }
 
-            int mid = (int) (Math.pow(2, n-1)/2);
-
-            if (k <= mid) {
-                return kthGrammar(n-1, k);
+            if( k % 2 == 1) {
+                return kthGrammar(n-1, (k+1)/2);
             } else {
-                return kthGrammar(n-1, k-mid) == 1 ? 0 : 1;
+                return kthGrammar(n-1, k/2) == 0 ? 1 : 0;
             }
         }
     }
