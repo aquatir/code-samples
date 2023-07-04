6205f7927e0e82a055a24e3fddbbcd4d9a46e867
pavel bukhmatov <58176914+yandex-praktikum@users.noreply.github.com>
2023-07-04 08:47:16 +0400

    add better 94 in-order traversal

--- code-sample-java/code-sample-java-algorithms-and-data-structures/src/main/java/codesample/leetcode/easy/_94_BinaryTreeInorderTraversal.java
+++ code-sample-java/code-sample-java-algorithms-and-data-structures/src/main/java/codesample/leetcode/easy/_94_BinaryTreeInorderTraversal.java
@@ -31,26 +31,21 @@ public class _94_BinaryTreeInorderTraversal {
 
 
     public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> result = new ArrayList<Integer>();
-
-        if (root == null) {
-            return result;
-        }
-
-        inorderTraversal(root.left, result);
-        result.add(root.val);
-        inorderTraversal(root.right, result);
-
+        List<Integer> result = new ArrayList<>();
+        inorderTraversal(root, result);
         return result;
     }
 
-    private void inorderTraversal(TreeNode node, List<Integer> result) {
-        if (node == null) {
+    public void inorderTraversal(TreeNode root, List<Integer> result) {
+        if (root == null) {
             return;
-        } else {
-            inorderTraversal(node.left, result);
-            result.add(node.val);
-            inorderTraversal(node.right, result);
+        }
+        if (root.left != null) {
+            inorderTraversal(root.left, result);
+        }
+        result.add(root.val);
+        if (root.right != null) {
+            inorderTraversal(root.right, result);
         }
     }
 
