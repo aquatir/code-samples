58422a6c01d65fcbf53fcbadb74390a93b4d0c48
pavel bukhmatov <58176914+yandex-praktikum@users.noreply.github.com>
2023-07-09 09:22:54 +0400

    add iterative solution for 102 — binary tree level order traversal

--- code-sample-java/code-sample-java-algorithms-and-data-structures/src/main/java/codesample/leetcode/medium/_102_BinaryTreeLevelOrderTraversal.java
+++ code-sample-java/code-sample-java-algorithms-and-data-structures/src/main/java/codesample/leetcode/medium/_102_BinaryTreeLevelOrderTraversal.java
@@ -1,5 +1,6 @@
 package codesample.leetcode.medium;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -31,32 +32,68 @@ public class _102_BinaryTreeLevelOrderTraversal {
         }
     }
 
+    // recursive
+//    public List<List<Integer>> levelOrder(TreeNode root) {
+//        List<List<Integer>> result = new ArrayList<List<Integer>>();
+//
+//        if (root == null) {
+//            return result;
+//        }
+//
+//        int level = 0;
+//        levelOrder(root, level, result);
+//
+//        return result;
+//    }
+//
+//    // add current value;
+//    // traverse left with level + 1
+//    // traverse right with level + 1
+//    public void levelOrder(TreeNode node, int level, List<List<Integer>> result) {
+//        if (node == null) {
+//            return;
+//        } else {
+//            if (result.size() <= level) {
+//                result.add(new ArrayList<Integer>());
+//            }
+//            result.get(level).add(node.val);
+//            levelOrder(node.left, level + 1, result);
+//            levelOrder(node.right, level + 1, result);
+//        }
+//    }
+
+    // iterative
     public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> result = new ArrayList<List<Integer>>();
-
+        var res = new ArrayList<List<Integer>>();
         if (root == null) {
-            return result;
+            return res;
         }
 
-        int level = 0;
-        levelOrder(root, level, result);
+        var curLevelNodes = new ArrayList<TreeNode>();
+        curLevelNodes.add(root);
 
-        return result;
-    }
+        // can't put 'null' in the queue to mark
+        // how can I possibly mark "level"?
+        //  one solution -> have a "cur" level LIST, generate another LIST in the end of iteration
+        //  no queue needed at all
+        //  but will need an array on TreeNodes.
+        while (!curLevelNodes.isEmpty()) {
+            var curLevelVals = new ArrayList<Integer>();
+            var nextLevelNodes = new ArrayList<TreeNode>();
 
-    // add current value;
-    // traverse left with level + 1
-    // traverse right with level + 1
-    public void levelOrder(TreeNode node, int level, List<List<Integer>> result) {
-        if (node == null) {
-            return;
-        } else {
-            if (result.size() <= level) {
-                result.add(new ArrayList<Integer>());
+            for (int i = 0; i < curLevelNodes.size(); i++) {
+                var node = curLevelNodes.get(i);
+                curLevelVals.add(node.val);
+                if (node.left != null) {
+                    nextLevelNodes.add(node.left);
+                }
+                if (node.right != null) {
+                    nextLevelNodes.add(node.right);
+                }
             }
-            result.get(level).add(node.val);
-            levelOrder(node.left, level + 1, result);
-            levelOrder(node.right, level + 1, result);
+            res.add(curLevelVals);
+            curLevelNodes = nextLevelNodes;
         }
+        return res;
     }
 }
