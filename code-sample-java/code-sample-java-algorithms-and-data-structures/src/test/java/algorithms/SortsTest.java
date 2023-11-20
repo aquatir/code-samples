0f0e03edbd4116e763bc06cc978ea0b7d644ec65
pavel bukhmatov <buhmatov@gmail.com>
2023-11-20 09:26:48 +0400

    fix sort tests

--- code-sample-java/code-sample-java-algorithms-and-data-structures/src/test/java/algorithms/SortsTest.java
+++ code-sample-java/code-sample-java-algorithms-and-data-structures/src/test/java/algorithms/SortsTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Random;
 
