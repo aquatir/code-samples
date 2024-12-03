d1d4910483781b4f1f13f7f4e793fc2396b2ba2d
pavel bukhmatov <buhmatov@gmail.com>
2024-12-03 22:58:03 +0400

    add even better solution for 1004

--- code-sample-java/code-sample-java-algorithms-and-data-structures/src/main/java/codesample/leetcode/medium/_1004_MaxConsecutiveOnesIII.java
+++ code-sample-java/code-sample-java-algorithms-and-data-structures/src/main/java/codesample/leetcode/medium/_1004_MaxConsecutiveOnesIII.java
@@ -10,32 +10,33 @@ public class _1004_MaxConsecutiveOnesIII {
 
     // Approach 1: sliding window but slightly magical "shrink" function
     // approach 2: below: a much clearer shrink function
-    //    public int longestOnes(int[] nums, int k) {
-//        int windowStart = 0;
-//        int max = 0;
-//        int maxOnesCount = 0;
-//
-//        for (int windowEnd = 0; windowEnd < nums.length; windowEnd++) {
-//            if (nums[windowEnd] == 1) {
-//                maxOnesCount++;
-//            }
-//
-//            // maybe shrink
-//            if (windowEnd - windowStart + 1 - maxOnesCount > k) {
-//                if (nums[windowStart] == 1) {
-//                    maxOnesCount--;
-//                }
-//                windowStart++;
-//            }
-//
-//            // max
-//            max = Math.max(max, windowEnd - windowStart + 1);
-//
-//        }
-//
-//        return max;
-//    }
     public int longestOnes(int[] nums, int k) {
+        int windowStart = 0;
+        int max = 0;
+        int maxOnesCount = 0;
+
+        for (int windowEnd = 0; windowEnd < nums.length; windowEnd++) {
+            if (nums[windowEnd] == 1) {
+                maxOnesCount++;
+            }
+
+            // maybe shrink
+            if (windowEnd - windowStart + 1 - maxOnesCount > k) {
+                if (nums[windowStart] == 1) {
+                    maxOnesCount--;
+                }
+                windowStart++;
+            }
+
+            // max
+            max = Math.max(max, windowEnd - windowStart + 1);
+
+        }
+
+        return max;
+    }
+
+    public int longestOnesSimpler(int[] nums, int k) {
         // flip 0 to 1 to create a larger consecutive array.
         // if we have a sliding window and there are [k] zeroes somewhere inside this window
         // we can count them all as consecutive.
@@ -77,6 +78,31 @@ public class _1004_MaxConsecutiveOnesIII {
         }
 
         return max;
+    }
+
+    public int longestOnesEvenSimpler(int[] nums, int k) {
+        var maxLength = 0;
+        var zeroesRemaining = k;
+        var left = 0;
+
+        for (int right = 0; right < nums.length; right++) {
+            // add to window
+            if (nums[right] == 0) {
+                zeroesRemaining--;
+            }
+
+            // make window valid
+            while (zeroesRemaining < 0) {
+                if (nums[left] == 0) {
+                    zeroesRemaining++;
+                }
+                left++;
+            }
+            // calculate
+            maxLength = Math.max(maxLength, right - left + 1);
+        }
+
+        return maxLength;
     }
 
     public static void main(String[] args) {
