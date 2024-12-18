68e3d66a1364ecf59db1dd834c7f9b2816cf729e
pavel bukhmatov <buhmatov@gmail.com>
2024-12-18 23:20:56 +0400

    add better 1189 solution

--- code-sample-java/code-sample-java-algorithms-and-data-structures/src/main/java/codesample/leetcode/easy/_1189_MaximumNumberOfBalloons.java
+++ code-sample-java/code-sample-java-algorithms-and-data-structures/src/main/java/codesample/leetcode/easy/_1189_MaximumNumberOfBalloons.java
@@ -8,6 +8,39 @@ import java.util.HashMap;
 public class _1189_MaximumNumberOfBalloons {
 
     public int maxNumberOfBalloons(String text) {
+        var b = 0;
+        var a = 0;
+        var l = 0;
+        var o = 0;
+        var n = 0;
+
+        for (var ch : text.toCharArray()) {
+            if (ch == 'b') {
+                b++;
+            } else if (ch == 'a') {
+                a++;
+            } else if (ch == 'l') {
+                l++;
+            } else if (ch == 'o') {
+                o++;
+            } else if (ch == 'n') {
+                n++;
+            }
+        }
+
+        // b = 1
+        // a = 1
+        // l == 2
+        // o == 2
+        // n = 1
+        var potential1 = Math.min(a, b);
+        potential1 = Math.min(potential1, n);
+        var potential2 = Math.min(l / 2, o / 2);
+
+        return Math.min(potential1, potential2);
+    }
+
+    public int maxNumberOfBalloonsFreqMap(String text) {
         var freq = new HashMap<Character, Integer>();
         for (var ch: text.toCharArray()) {
             freq.put(ch, freq.getOrDefault(ch, 0) + 1);
