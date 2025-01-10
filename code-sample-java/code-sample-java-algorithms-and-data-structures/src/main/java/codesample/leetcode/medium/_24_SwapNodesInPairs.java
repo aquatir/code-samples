eeccc5830e9942574366f134bae3cfa1dc81c438
pavel bukhmatov <buhmatov@gmail.com>
2025-01-10 23:26:23 +0400

    add much simpler solution for 24 swap nodes in pairs

--- code-sample-java/code-sample-java-algorithms-and-data-structures/src/main/java/codesample/leetcode/medium/_24_SwapNodesInPairs.java
+++ code-sample-java/code-sample-java-algorithms-and-data-structures/src/main/java/codesample/leetcode/medium/_24_SwapNodesInPairs.java
@@ -22,8 +22,48 @@ public class _24_SwapNodesInPairs {
         }
     }
 
-
     public ListNode swapPairs(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        if (head.next == null) {
+            return head;
+        }
+
+        var cur = head;
+        var newHead = head.next;  // new head is always going to be this after swap
+        ListNode prev = null;
+        // move by two nodes at once.
+        while (cur != null && cur.next != null) {
+            var afterRotation = rotate(prev, cur, cur.next);
+            prev = afterRotation;
+            cur = afterRotation.next;
+        }
+
+        return newHead;
+    }
+
+    // rotate 2 nodes, update prev if possible, return a node which was moved (e.g. for a-b initial, result is b-a, return node a)
+    private ListNode rotate(ListNode prev, ListNode a, ListNode b) {
+        // before
+        // [prev]  -  a   -   b    - c
+        //
+        // becomes this
+        // [prev]  -  b  -  a   -  c; return node a (to set it as prev)
+        // + handle prev if it's present. Prev will point to b
+
+        var c = b.next;
+        a.next = c;
+        b.next = a;
+        if (prev != null) {
+            prev.next = b;
+        }
+        return a;
+    }
+
+
+    // tricky recursion solution
+    public ListNode swapPairsRecursion(ListNode head) {
         if (head == null) {
             return null;
         }
@@ -36,7 +76,7 @@ public class _24_SwapNodesInPairs {
         head.next = next.next;
         next.next = head;
 
-        head.next = swapPairs(head.next);
+        head.next = swapPairsRecursion(head.next);
 
         return next;  // next is new head
     }
